d4c3954f78edf5e621a89daea90c339f93208c1d
Andy-Shao <hj13035952619@126.com>
2026-01-30 14:22:12 +0000

    add t-SNE analysis

--- lib/component.py
+++ lib/component.py
@@ -3,6 +3,25 @@ import numpy as np
 import torch
 from torch import nn
 
+class AudioPadding(nn.Module):
+    def __init__(self, max_length:int, sample_rate:int, random_shift:bool=False):
+        super(AudioPadding, self).__init__()
+        self.max_length = max_length
+        self.random_shift = random_shift
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        from torch.nn.functional import pad
+        l = self.max_length - x.shape[1]
+        if l > 0:
+            if self.random_shift:
+                head = random.randint(0, l)
+                tail = l - head
+            else:
+                head = l // 2
+                tail = l - head
+            x = pad(x, (head, tail), mode='constant', value=0.)
+        return x
+
 class Components(nn.Module):
     def __init__(self, transforms: list) -> None:
         super().__init__()
--- lib/dataset.py
+++ lib/dataset.py
@@ -1,6 +1,25 @@
+from dataclasses import dataclass
+
 import torch
 from torch import nn
 from torch.utils.data import Dataset
+
+class Subset(Dataset):
+    def __init__(self, dataset: Dataset, label_list:list[int]):
+        super().__init__()
+        self.dataset = dataset
+        self.label_list = label_list
+        self.set_IDs = self.__data_scanning__()
+
+    def __data_scanning__(self) -> list[int]:
+        return [idx for idx, (feature, label) in enumerate(self.dataset) if label in self.label_list]
+    
+    def __len__(self):
+        return len(self.set_IDs)
+
+    def __getitem__(self, index):
+        return self.dataset[self.set_IDs[index]]
+
 
 class TransferDataset(Dataset):
     def __init__(self, dataset: Dataset, data_tf:nn.Module=None, label_tf:nn.Module=None, device='cpu', keep_cpu=True) -> None:
