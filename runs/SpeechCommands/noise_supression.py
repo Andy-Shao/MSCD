284248e0f103c9cfe426f1fcc8af1d454781b729
Andy-Shao <hj13035952619@126.com>
2026-01-30 09:04:13 +0000

    add noise supression

--- runs/SpeechCommands/noise_supression.py
+++ runs/SpeechCommands/noise_supression.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import pyrnnoise
+import numpy as np
 
 import torch
 import torchaudio
@@ -27,12 +29,30 @@ if __name__ == '__main__':
     sc2_c = SpeechCommandsV2C(
         root_path=args.dataset_root_path, corruption_level='L2', corruption_type='WHN',
     )
-    wavform, sample_rate = sc2_c[0]
-    print(f'wavform shape: {wavform.shape}, sample rate: {sample_rate}')
+    noisy, sample_rate = sc2_c[0]
+    print(f'noisy wavform shape: {noisy.shape}, sample rate: {sample_rate}')
 
     torchaudio.save(
-        uri=os.path.join('/root', 'output', 'noise_case.wav'),
-        src=wavform.detach(), 
+        uri=os.path.join('/root', 'output', 'noisy.wav'),
+        src=noisy.detach(), 
+        sample_rate=sample_rate,
+        encoding='PCM_S',
+        bits_per_sample=16
+    )
+
+    denoiser = pyrnnoise.RNNoise(sample_rate=sample_rate)
+    noise_np = noisy * 32768.0
+    noise_np = noise_np.clip(-32768.0, 32767.0)
+    noise_np = noise_np.numpy()
+    noise_np = noise_np.astype(np.int16)
+    out = [denoise_audio for speech_prob, denoise_audio in denoiser.denoise_chunk(noise_np, partial=True)]
+    clean_wav = torch.tensor(np.concat(out, axis=1)).to(torch.float32)/32768.0
+
+    print(f'clean wavform shape:{clean_wav.shape}')
+
+    torchaudio.save(
+        uri=os.path.join('/root', 'output', 'clean.wav'),
+        src=clean_wav.detach(),
         sample_rate=sample_rate,
         encoding='PCM_S',
         bits_per_sample=16
