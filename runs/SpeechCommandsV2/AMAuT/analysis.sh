6cfd892649bddbc603ad6bf1552ad6e55436ebfb
Andy-Shao <hj13035952619@126.com>
2026-01-31 12:51:09 +0000

    optimize code

--- runs/SpeechCommandsV2/AMAuT/analysis.sh
+++ runs/SpeechCommandsV2/AMAuT/analysis.sh
@@ -3,5 +3,5 @@ BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m runs.SpeechCommandsV2.AMAuT.noise_supression_analysis --dataset 'SpeechCommandsV2' \
     --dataset_root_path $BASE_PATH'/data/SpeechCommandsV2-C' --batch_size 32 \
-    --output_file_name 'Denoiser_DNS64_analysis.csv'\
+    --output_file_name 'Denoiser_DNS64_analysis.csv' --cache_path $BASE_PATH'/tmp/SC2-C_DNS64_cache' \
     --orig_wght_pth $BASE_PATH'/result/SpeechCommandsV2/AMAuT/train'
--- runs/SpeechCommandsV2/AMAuT/noise_supression_analysis.py
+++ runs/SpeechCommandsV2/AMAuT/noise_supression_analysis.py
@@ -11,7 +11,7 @@ from lib.corruption import corruption_meta
 from lib.spSet import SpeechCommandsV2C
 from lib.dataset import TransferDataset
 from lib.component import Components, AmplitudeToDB, FrequenceTokenTransformer
-from noise_suppression.DNS import DNSnoise
+from noise_suppression.DNS import DNS_cache_set
 from .utils import build_model, inference, load_weight
 # from ..utils import denormalize, normalize
 
@@ -25,6 +25,7 @@ if __name__ == '__main__':
     ap.add_argument('--batch_size', type=int, default=64)
     ap.add_argument('--orig_wght_pth', type=str)
     ap.add_argument('--adpt_wght_path', type=str)
+    ap.add_argument('--cache_path', type=str)
 
     args = ap.parse_args()
     if args.dataset == 'SpeechCommandsV2':
@@ -51,18 +52,14 @@ if __name__ == '__main__':
     args.target_length=104
     records = pd.DataFrame(columns=['Dataset',  'Algorithm', 'Param No.', 'Corruption', 'Denoiser', 'Raw_accu', 'Denoisy_accu', 'Improved'])
     corruption_metas = corruption_meta(corruption_types=corruption_types, corruption_levels=corruption_levels)
-    aut, clsf = build_model(args=args)
-    load_weight(args=args, aut=aut, clsf=clsf, mode='origin')
-    param_no = count_ttl_params(aut) + count_ttl_params(clsf)
-
+    noisy_sets=[]; clean_sets=[]
     for idx, cmeta in enumerate(corruption_metas):
-        print(f'{idx+1}/{len(corruption_metas)}: {args.dataset} {cmeta.type}-{cmeta.level} analyzing...')
-        cs2_c = SpeechCommandsV2C(
+        print(f'{idx+1}/{len(corruption_metas)}: {args.dataset} {cmeta.type}-{cmeta.level} set generating...')
+        sc2_c = SpeechCommandsV2C(
             root_path=args.dataset_root_path, corruption_level=cmeta.level, corruption_type=cmeta.type,
         )
-
-        org_set = TransferDataset(
-            dataset=cs2_c, data_tf=Components(transforms=[
+        noisy_set = TransferDataset(
+            dataset=sc2_c, data_tf=Components(transforms=[
                 MelSpectrogram(
                     sample_rate=args.sample_rate, n_fft=n_fft, n_mels=args.n_mels, win_length=win_length,
                     hop_length=hop_length, mel_scale=mel_scale
@@ -71,15 +68,13 @@ if __name__ == '__main__':
                 FrequenceTokenTransformer()
             ])
         )
-        org_loader = DataLoader(
-            dataset=org_set, batch_size=args.batch_size, shuffle=False, drop_last=False, 
-            num_workers=args.num_workers
-        )
-
-        adpt_set = TransferDataset(
-            dataset=cs2_c, data_tf=Components(transforms=[
-                # RNNoiseTransform(sample_rate=args.sample_rate, denormalize=denormalize, normalize=normalize),
-                DNSnoise(),
+        noisy_sets.append(noisy_set)
+        clean_set = TransferDataset(
+            dataset=DNS_cache_set(
+                dataset=sc2_c, device=args.device, sample_rate=args.sample_rate, 
+                cache_path=os.path.join(args.cache_path, cmeta.type, cmeta.level)
+            ),
+            data_tf=Components(transforms=[
                 MelSpectrogram(
                     sample_rate=args.sample_rate, n_fft=n_fft, win_length=win_length, hop_length=hop_length,
                     n_mels=args.n_mels, mel_scale=mel_scale
@@ -88,17 +83,34 @@ if __name__ == '__main__':
                 FrequenceTokenTransformer()
             ])
         )
-        adpt_loader = DataLoader(
-            dataset=adpt_set, batch_size=args.batch_size, shuffle=False, drop_last=False, 
+        clean_sets.append(clean_set)
+
+    aut, clsf = build_model(args=args)
+    load_weight(args=args, aut=aut, clsf=clsf, mode='origin')
+    param_no = count_ttl_params(aut) + count_ttl_params(clsf)
+
+    for idx, cmeta in enumerate(corruption_metas):
+        print(f'{idx+1}/{len(corruption_metas)}: {args.dataset} {cmeta.type}-{cmeta.level} analyzing...')
+        
+        noisy_set = noisy_sets[idx]
+        clean_set = clean_sets[idx]
+        
+        noisy_loader = DataLoader(
+            dataset=noisy_set, batch_size=args.batch_size, shuffle=False, drop_last=False, 
             num_workers=args.num_workers
         )
 
-        print('Origin evaluation')
-        org_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=org_loader)
-        print('Adaptation evaluation')
-        adpt_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=adpt_loader)
-        print(f'Original accuracy: {org_accu:.4f}, adaptation accuracy: {adpt_accu:.4f}')
-        records.loc[len(records)] = [args.dataset, args.arch, param_no, f'{cmeta.type}-{cmeta.level}', 'DNS64', org_accu, adpt_accu, adpt_accu-org_accu]
+        clean_loader = DataLoader(
+            dataset=clean_set, batch_size=args.batch_size, shuffle=False, drop_last=False, 
+            num_workers=args.num_workers
+        )
+
+        print('Noisy set evaluation')
+        noisy_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=noisy_loader)
+        print('Clean set evaluation')
+        clean_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=clean_loader)
+        print(f'Noisy set accuracy: {noisy_accu:.4f}, Clean set accuracy: {clean_accu:.4f}')
+        records.loc[len(records)] = [args.dataset, args.arch, param_no, f'{cmeta.type}-{cmeta.level}', 'DNS64', noisy_accu, clean_accu, clean_accu-noisy_accu]
     records.to_csv(os.path.join(args.output_path, args.output_file_name))
 
     print('END!')
