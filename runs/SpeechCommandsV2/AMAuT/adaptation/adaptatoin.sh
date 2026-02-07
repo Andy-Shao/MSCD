ab8cb9362bad5a03c53029765e21bfa530ae2b52
Andy-Shao <hj13035952619@126.com>
2026-02-07 10:17:16 +0000

    optimization

--- runs/SpeechCommandsV2/AMAuT/adaptation/adaptatoin.sh
+++ runs/SpeechCommandsV2/AMAuT/adaptation/adaptatoin.sh
@@ -4,7 +4,7 @@ BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.SpeechCommandsV2.AMAuT.adaptation.teacher_adaptation --dataset 'SpeechCommandsV2' \
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' --batch_size 32 \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --corruption_level 'L2' --max_epoch 10 --lr '1e-4' --num_of_shft 6 --lr_gamma 30 \
+    --corruption_level 'L2' --max_epoch 20 --lr '1e-4' --num_of_shft 5 --lr_gamma 30 \
     --aut_lr_decay 0.55 --forbid_ls 'TST' --fail_coll_lim 6 \
     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.0, "END2":1.0, "ENSC":1.0, "PSH":1.0, "TST":1.5}' \
     --adpt_wght_path $BASE_PATH'/result/SpeechCommandsV2/AMAuT/TTDA' --wandb
