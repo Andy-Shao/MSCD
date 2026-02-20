425481db84886846d0d26cd3a697c0dbfdeb98b9
Andy-Shao <hj13035952619@126.com>
2026-02-20 14:36:19 +0000

    adjust code

--- runs/ReefSet/AMAuT/adaptation/adaptation.sh
+++ runs/ReefSet/AMAuT/adaptation/adaptation.sh
@@ -5,6 +5,6 @@ python -m runs.ReefSet.AMAuT.adaptation.teach_adapt --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
     --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr '1e-4' \
-    --forbid_ls 'TST' \
+    --forbid_ls 'TST' --aut_lr_decay 0.55 --lr_gamma 30 --num_of_shft 5 \
     --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.2, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' 
+    --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' --wandb
--- runs/ReefSet/AMAuT/adaptation/teach_adapt.py
+++ runs/ReefSet/AMAuT/adaptation/teach_adapt.py
@@ -104,7 +104,7 @@ def pseudo_labeling(
                 y_s += nn.functional.softmax(outputs, dim=1) * args.elect_weights[corruption_type]
             if i == 0: output_cache[corruption_type] = [outputs]
             else: output_cache[corruption_type].append(outputs)
-        _, final_preds = torch.max(final_preds, dim=1)
+        _, final_preds = torch.max(y_s, dim=1)
         y_true.append(indexes2oneHot(labels=labels, class_num=args.class_num))
         y_score.append(y_s)
         if i==0: 
