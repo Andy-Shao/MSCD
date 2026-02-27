4e0f0888517d5d3adf0730aec4988079700ba211
Andy-Shao <hj13035952619@126.com>
2026-02-27 13:40:28 +0000

    clean code

--- runs/ReefSet/AMAuT/adaptation/adaptation.sh
+++ runs/ReefSet/AMAuT/adaptation/adaptation.sh
@@ -14,7 +14,7 @@ BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.ReefSet.AMAuT.adaptation.std_adapt --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr 1e-4 --pseudo_threshold 5.5 \
+    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr 1e-4 --pseudo_threshold 7.5 \
     --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.2, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
     --adpt_wght_pth './result/ReefSet/AMAuT/Teach-Adapt' \
-    --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train'
+    --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' --wandb
