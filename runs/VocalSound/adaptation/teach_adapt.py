9f131a50260e3497279a18283f2d1eafb0ad1150
Andy-Shao <hj13035952619@126.com>
2026-03-04 13:27:32 +0000

    add teacher adaptation for VS-C

--- runs/VocalSound/adaptation/teach_adapt.py
+++ runs/VocalSound/adaptation/teach_adapt.py
@@ -14,12 +14,69 @@ from torchaudio.transforms import MelSpectrogram
 from lib import constants
 from lib.utils import make_unless_exits, print_argparse
 from lib.loss import CrossEntropyLabelSmooth
-from lib.optimizer import build_optimizer
+from lib.optimizer import build_optimizer, lr_scheduler
 from lib.corruption import CorruptionMeta
 from lib.component import Components, AmplitudeToDB, FrequenceTokenTransformer
 from lib.component import MelSpectrogramPadding
 from lib.spSet import VocalSoundC
-from ..util import build_model, load_weight, teach_inference
+from lib.dataset import IdxSet, PseudoLabelSet, Subset
+from lib.adaptation import collect_worst_item
+from ..util import build_model, load_weight, teach_inference, store_weight
+
+def pseudo_labeling(
+    args:argparse.Namespace, auts:list[nn.Module], clsfs:list[nn.Module], corruption_types:list[str], 
+    data_tfs:list[nn.Module], step:int, logger
+):
+    print("Pseudo-labeling...")
+    for aut in auts: aut.eval()
+    for clsf in clsfs: clsf.eval()
+    vs_set = VocalSoundC(
+        root_path=args.adpt_set_path, corruption_level=args.corruption_level, corruption_type=corruption_types,
+        data_tf=data_tfs
+    )
+    vs_set = IdxSet(dataset=vs_set)
+    vs_loader = DataLoader(
+        dataset=vs_set, batch_size=args.batch_size, shuffle=False, drop_last=False, 
+        num_workers=args.num_workers
+    )
+
+    ttl_corr, ttl_size = 0., 0.
+    output_cache = {k:[] for k in corruption_types}
+    for i, data in tqdm(enumerate(vs_loader), total=len(vs_loader)):
+        labels = data[-1]
+        idxs = data[0]
+        for j in range(1, len(data)-1):
+            features = data[j].to(args.device)
+            aut, clsf = auts[j-1], clsfs[j-1]
+            corruption_type = corruption_types[j-1]
+            with torch.inference_mode():
+                outputs = clsf(aut(features)[1])
+                outputs = outputs.detach().cpu()
+            preds = nn.functional.softmax(outputs, dim=1) * args.elect_weights[corruption_type]
+            if j==1: final_preds = preds
+            else: final_preds += preds
+            output_cache[corruption_type].append(outputs)
+        _, final_preds = torch.max(final_preds, dim=1)
+        ttl_corr += (final_preds==labels).sum().item()
+        ttl_size += labels.shape[0]
+        if i == 0:
+            pred_cache = [final_preds]
+            idx_cache = [idxs]
+        else:
+            pred_cache.append(final_preds)
+            idx_cache.append(idxs)
+    pseudo_accu = ttl_corr/ttl_size
+    print(f'Teacher election pseudo-labeling accuracy is: {pseudo_accu:.4f}')
+    logger.log(data={'Adaptation/pseudo-labeling Accuracy': pseudo_accu}, step=step)
+
+    # Merging output cache
+    tmp = {}
+    for key, value in output_cache.items():
+        tmp[key] = torch.concat(value, dim=0)
+    output_cache = tmp
+    pred_cache = torch.concat(pred_cache, dim=0)
+    idx_cache = torch.concat(idx_cache, dim=0)
+    return output_cache, pred_cache, idx_cache, pseudo_accu
 
 def teacher_accu_analyzing(
     args:argparse.Namespace, auts:list[nn.Module], clsfs:list[nn.Module], corruption_types:list[str],
@@ -130,7 +187,6 @@ if __name__ == '__main__':
 
     print("Initialization...")
     auts, clsfs = [], []
-    max_pseudo_accu = 0.
     optimizers = []
     loss_fun = CrossEntropyLabelSmooth(num_classes=args.class_num, use_gpu=torch.cuda.is_available())
     for corruption_type in tqdm(corruption_types):
@@ -151,13 +207,67 @@ if __name__ == '__main__':
         FrequenceTokenTransformer(),
     ])] * len(corruption_types)
 
+    max_pseudo_accu = 0.
     for epoch in range(args.max_epoch+1):
         print(f'Epoch: {epoch+1}/{args.max_epoch} processing...')
         teacher_accu_analyzing(
             args=args, auts=auts, clsfs=clsfs, corruption_types=corruption_types, 
             data_tfs=data_tfs, step=epoch, logger=wandb_run
         )
-        exit()
+        output_cache, pred_cache, idx_cache, pseudo_accu = pseudo_labeling(
+            args=args, auts=auts, clsfs=clsfs, corruption_types=corruption_types, 
+            data_tfs=data_tfs, step=epoch, logger=wandb_run
+        )
+        if max_pseudo_accu <= pseudo_accu:
+            max_pseudo_accu = pseudo_accu
+            for i, corruption_type in enumerate(corruption_types):
+                store_weight(
+                    args=args, aut=auts[i], clsf=clsfs[i], mode='adaptation', 
+                    metaInfo=CorruptionMeta(type=corruption_type, level=args.corruption_level),
+                    root_path=args.output_path
+                )
+        worst_list, shft_typs = collect_worst_item(
+            args=args, corruption_types=corruption_types, idx_cache=idx_cache, pred_cache=pred_cache, 
+            output_cache=output_cache, step=epoch, logger=wandb_run, feature_label=False
+        )
+        if epoch == args.max_epoch: break
+        print('Adapting...')
+        for aut in auts: aut.train()
+        for clsf in clsfs: clsf.train()
+        for i, corruption_type in tqdm(enumerate(corruption_types), total=len(corruption_types), position=0):
+            adpt_set = VocalSoundC(
+                root_path=args.adpt_set_path, corruption_level=args.corruption_level, corruption_type=corruption_type,
+                data_tf=data_tfs[0]
+            )
+            adpt_set = PseudoLabelSet(dataset=adpt_set, pseudo_labels=worst_list[corruption_type], label_position=1)
+            adpt_set = Subset(dataset=adpt_set, id_list=list(worst_list[corruption_type].keys()))
 
+            adpt_loader = DataLoader(
+                dataset=adpt_set, batch_size=args.batch_size, shuffle=True, drop_last=False, 
+                num_workers=args.num_workers
+            )
+            teach_aut, teach_clsf = auts[i], clsfs[i]
+            optimizer = optimizers[i]
+
+            for features, labels in tqdm(adpt_loader, desc=f'{corruption_type}-{args.corruption_level}', position=1, leave=False):
+                if corruption_type not in shft_typs: break
+                if corruption_type in args.forbid_ls: break
+                features, labels = features.to(args.device), labels.to(args.device)
+                if features.shape[0] == 1:
+                    features = features.repeat(4, 1, 1)
+                    labels = labels.repeat(4)
+
+                outputs = teach_clsf(teach_aut(features)[1])
+                clsf_loss = loss_fun(outputs, labels)
+
+                optimizer.zero_grad()
+                clsf_loss.backward()
+                optimizer.step()
+            
+            if epoch % args.interval == 0:
+                lr_scheduler(
+                    optimizer=optimizer, epoch=epoch+1, lr_cardinality=args.lr_cardinality, 
+                    gamma=args.lr_gamma, threshold=args.lr_threshold, momentum=args.lr_momentum
+                )
     wandb_run.finish()
     print('END!')
