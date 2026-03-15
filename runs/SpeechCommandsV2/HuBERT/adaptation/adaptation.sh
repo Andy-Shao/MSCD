7c1104844dbbbaa1a78a1c1b3cece8308a1f9cbf
Andy-Shao <hj13035952619@126.com>
2026-03-15 09:46:22 +0000

    optimization

--- runs/SpeechCommandsV2/HuBERT/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/HuBERT/adaptation/adaptation.sh
@@ -5,7 +5,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.SpeechCommandsV2.HuBERT.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --num_of_shft 3  \
+    --batch_size 32 --corruption_level 'L2' --max_epoch 30 --num_of_shft 4  \
     --fail_coll_lim 6 \
     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.0, "END2":1.0, "ENSC":1.0, "PSH":1.0, "TST":1.0}' \
     --lrs '{"WHN":7e-5, "ENQ":7e-5, "END1":7e-5, "END2":7e-5, "ENSC":1e-4, "PSH":1e-4, "TST":7e-5}' \
