46e7eeea101ea2fd2eac7369060da0a3e5bf3e8f
Andy-Shao <hj13035952619@126.com>
2026-03-18 16:41:50 +0000

    optimization

--- runs/ReefSet/HuBERT/adaptation/adaptation.sh
+++ runs/ReefSet/HuBERT/adaptation/adaptation.sh
@@ -6,6 +6,6 @@ python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
     --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 3 --max_epoch 20 \
-    --lr 1e-4 --lr_momentum 0.75 \
+    --lr 1e-4 --lr_momentum 0.75 --lr_gamma 30 \
     --elect_weights '{"WHN":1.0, "ENQ":1.2, "END1":1.0, "END2":1.0, "ENSC":1.0, "PSH":3.0, "TST":1.5}' \
     --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
