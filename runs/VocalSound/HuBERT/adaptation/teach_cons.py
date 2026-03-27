5ea422ad76ad47b41eab7d2f36f54ef6137b5e0b
Andy-Shao <hj13035952619@126.com>
2026-03-27 11:06:19 +0000

    optimization

--- runs/VocalSound/HuBERT/adaptation/teach_cons.py
+++ runs/VocalSound/HuBERT/adaptation/teach_cons.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
         optimizers.append(optimizer)
     data_tfs = [ReduceChannel()] * len(corruption_types)
 
-    max_pl_accu = 0.
+    # max_pl_accu = 0.
     for epoch in range(args.max_epoch+1):
         print(f'Epoch: {epoch+1}/{args.max_epoch} processing...')
         teacher_accu_analyzing(
@@ -198,9 +198,9 @@ if __name__ == '__main__':
             args=args, hubs=teach_hubs, clsfs=teach_clsfs, corruption_types=corruption_types, data_tfs=data_tfs,
             step=epoch, logger=wandb_run
         )
-        if max_pl_accu <= pseudo_accu:
-            max_pl_accu = pseudo_accu
-            for i, corruption_type in enumerate(corruption_types):
+        # if max_pl_accu <= pseudo_accu:
+        #     max_pl_accu = pseudo_accu
+        for i, corruption_type in enumerate(corruption_types):
                 store_weight(
                     args=args, hubert=teach_hubs[i], clsf=teach_clsfs[i], mode='adaptation', 
                     metaInfo=CorruptionMeta(type=corruption_type, level=args.corruption_level),
