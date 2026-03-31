4ed94738392b909e9b4551d18fe4ded4ac3e300a
Andy-Shao <hj13035952619@126.com>
2026-03-31 16:00:54 +0000

    optimization

--- runs/UrbanSound8K/HuBERT/adaptation/adaptation.sh
+++ runs/UrbanSound8K/HuBERT/adaptation/adaptation.sh
@@ -12,8 +12,8 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.UrbanSound8K.HuBERT.adaptation.kd --dataset 'UrbanSound8K' \
     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 30 --lr '1e-4' \
-    --ctr_rt 0.0 --ctr_dist 'sq_l2' --pseudo_threshold 2.95 \
+    --batch_size 32 --corruption_level 'L2' --max_epoch 35 --lr '1e-4' \
+    --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 2.95 \
     --elect_weights '{"WHN":1.0, "ENSC":0.83, "PSH":1.0, "TST":1.0}' \
     --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/train' \
     --adpt_wght_pth './result/UrbanSound8K/HuBERT/Teach-Cons' --wandb
