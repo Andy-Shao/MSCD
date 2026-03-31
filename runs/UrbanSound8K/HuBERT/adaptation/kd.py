32e34c811b87e517ec02f8b21a64e4bba84d071e
Andy-Shao <hj13035952619@126.com>
2026-03-31 14:55:35 +0000

    optimization

--- runs/UrbanSound8K/HuBERT/adaptation/kd.py
+++ runs/UrbanSound8K/HuBERT/adaptation/kd.py
@@ -20,6 +20,7 @@ from lib.enSet import UrbanSound8KC
 from lib.dataset import IdxSet, PseudoLabelSet
 from lib.optimizer import build_optimizer, lr_scheduler
 from lib.loss import ContrastiveLoss
+from lib.adaptation import hub_clsf_freeze
 from ..utils import build_model, mlt_inference
 from HuBERT.lib.utils import load_weight, store_weight
 
@@ -233,7 +234,7 @@ if __name__ == '__main__':
         if epoch >= args.max_epoch: break
         print('Adaptating...')
         std_hub.train(); std_clsf.train()
-        # hub_clsf_freeze(model=std_clsf)
+        hub_clsf_freeze(model=std_clsf)
         ttl_loss = 0.; ttl_clsf_loss = 0.; ttl_ctr_loss = 0.
         for adpt_data in tqdm(adpt_loader):
             labels = adpt_data[-1].to(args.device)
