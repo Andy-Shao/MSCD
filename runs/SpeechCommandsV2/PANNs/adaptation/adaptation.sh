245df4e6d5f932bd48e20dfbfad40b740a2653ee
Andy-Shao <hj13035952619@126.com>
2026-04-06 15:06:12 +0000

    optimization

--- runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
@@ -46,7 +46,7 @@ python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
     --corruption_type 'PSH' --corruption_level 'L2' --batch_size 70 --max_epoch 60 \
-    --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.0 --mse_rate 0.0 \
+    --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --mse_rate 0.0 \
     --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.6 --lr_cardinality 80 \
     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
 
