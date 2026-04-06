f738cd08e7f34d1de09099c88e5331514cffc809
Andy-Shao <hj13035952619@126.com>
2026-04-06 09:46:29 +0000

    add END2 for PANNs on SC2-c

--- runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
@@ -18,10 +18,18 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.6 \
 #     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
 
+# python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+#     --corruption_type 'END1' --corruption_level 'L2' --batch_size 70 --max_epoch 25 \
+#     --lr 5e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.1 --mse_rate 0.1 \
+#     --lr_momentum 0.70 --pan_lr_decay 1.0 --gent_q 1.6 \
+#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
+
 python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --corruption_type 'END1' --corruption_level 'L2' --batch_size 70 --max_epoch 25 \
-    --lr 5e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.1 --mse_rate 0.1 \
-    --lr_momentum 0.70 --pan_lr_decay 1.0 --gent_q 1.6 \
+    --corruption_type 'END2' --corruption_level 'L2' --batch_size 70 --max_epoch 25 \
+    --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --mse_rate 0.0 \
+    --lr_momentum 0.75 --pan_lr_decay 1.0 --gent_q 1.6 \
     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
