dda5060441ac00397797c2f81496294d30e0a8e6
Andy-Shao <hj13035952619@126.com>
2026-04-07 11:15:48 +0000

    add PANNs teach consensus on SC2-C

--- runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
@@ -50,10 +50,18 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.6 --lr_cardinality 80 \
 #     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
 
-python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
+# python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+#     --corruption_type 'TST' --corruption_level 'L2' --batch_size 70 --max_epoch 25 \
+#     --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.0 --mse_rate 0.1 \
+#     --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.6 \
+#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
+
+# Teacher Consensus
+python -m runs.SpeechCommandsV2.PANNs.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --corruption_type 'TST' --corruption_level 'L2' --batch_size 70 --max_epoch 25 \
-    --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.0 --mse_rate 0.1 \
-    --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.6 \
-    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
+    --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 3 --max_epoch 2 --lr 1e-4 \
+    --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.0, "END2":1.0, "ENSC":1.0, "PSH":1.0, "TST":1.3}' \
+    --adpt_wght_pth './result/SpeechCommandsV2/PANNs/TTDA'
---
+++ runs/SpeechCommandsV2/PANNs/adaptation/teach_cons.py
@@ -0,0 +1,258 @@
+import argparse
+import os
+import json
+import numpy as np
+import wandb
+import random
+from tqdm import tqdm
+
+import torch 
+from torch import nn
+from torch.utils.data import DataLoader
+from torchaudio.transforms import Resample
+
+from lib import constants
+from lib.utils import make_unless_exits, print_argparse
+from lib.loss import CrossEntropyLabelSmooth
+from lib.corruption import CorruptionMeta
+from lib.optimizer import build_optimizer, lr_scheduler
+from lib.component import Components, ReduceChannel, AudioPadding
+from lib.spSet import SpeechCommandsV2C
+from lib.dataset import IdxSet, PseudoLabelSet, Subset
+from lib.adaptation import collect_worst_item
+from ..utils import build_model, teach_inference
+from PANNs.lib.utils import load_weight, store_weight
+
+def pseudo_labeling(
+        args:argparse.Namespace, pans:list[nn.Module], clsfs:list[nn.Module], data_tfs:list[nn.Module],
+        corruption_types:list[str], step:int, logger
+    ):
+    print("Pseudo-labeling...")
+    for pan in pans: pan.eval()
+    for clsf in clsfs: clsf.eval()
+    ttl_corr, ttl_size = 0., 0.
+    output_cache = {}
+
+    adpt_set = SpeechCommandsV2C(
+        root_path=args.adpt_set_path, corruption_level=args.corruption_level, corruption_type=corruption_types,
+        data_tf=data_tfs
+    )
+    adpt_set = IdxSet(dataset=adpt_set)
+    adpt_loader = DataLoader(
+        dataset=adpt_set, batch_size=args.batch_size, shuffle=False, drop_last=False, num_workers=args.num_workers
+    )
+
+    for j, data in tqdm(enumerate(adpt_loader), total=len(adpt_loader)):
+        labels = data[-1]
+        idxs = data[0]
+        for i in range(1, len(data)-1):
+            features = data[i].to(args.device)
+            pan = pans[i-1]
+            clsf = clsfs[i-1]
+            corruption_type = corruption_types[i-1]
+            with torch.inference_mode():
+                outputs = clsf(pan(features)['embedding'])
+                outputs = outputs.detach().cpu()
+            preds = nn.functional.softmax(outputs, dim=1) * args.elect_weights[corruption_type]
+            if i == 1: final_preds = preds
+            else: final_preds += preds
+            if j == 0: output_cache[corruption_type] = [outputs]
+            else: output_cache[corruption_type].append(outputs)
+        _, final_preds = torch.max(final_preds, dim=1)
+        ttl_corr += (final_preds==labels).sum().item()
+        ttl_size += labels.shape[0]
+        if j == 0: 
+            pred_cache = [final_preds]
+            idx_cache = [idxs]
+        else: 
+            pred_cache.append(final_preds)
+            idx_cache.append(idxs)
+    pseudo_accu = ttl_corr/ttl_size
+    print(f'Teacher election pseudo-labeling accuracy is: {pseudo_accu:.4f}')
+    logger.log(data={'Adaptation/pseudo-labeling Accuracy': pseudo_accu}, step=step)
+
+    # Merging output cache
+    tmp = {}
+    for key, value in output_cache.items():
+        tmp[key] = torch.concat(value, dim=0)
+    output_cache = tmp
+    pred_cache = torch.concat(pred_cache, dim=0)
+    idx_cache = torch.concat(idx_cache, dim=0)
+    return output_cache, pred_cache, idx_cache, pseudo_accu
+
+def teacher_accu_analyzing(
+        args:argparse.Namespace, pans:list[nn.Module], clsfs:list[nn.Module], corruption_types:list[str],
+        data_tfs:list[nn.Module], step:int, logger
+    ):
+    print('Teacher accuracy analyzing...')
+    print('Adaptation Set')
+    adpt_set = SpeechCommandsV2C(
+        root_path=args.adpt_set_path, corruption_level=args.corruption_level, corruption_type=corruption_types,
+        data_tf=data_tfs
+    )
+    adpt_loader = DataLoader(
+        dataset=adpt_set, batch_size=args.batch_size, shuffle=False, drop_last=False, num_workers=args.num_workers
+    )
+    adpt_accus = teach_inference(
+        args=args, corruption_types=corruption_types, pans=pans, clsfs=clsfs, data_loader=adpt_loader
+    )
+    print({k:round(v, ndigits=4) for k,v in adpt_accus.items()})
+    logger.log(data={f'Adaptation/{corruption_type} Accuracy': adpt_accus[corruption_type] for corruption_type in corruption_types}, step=step)
+
+    print('Evaluation Set')
+    eval_set = SpeechCommandsV2C(
+        root_path=args.eval_set_path, corruption_level=args.corruption_level, corruption_type=corruption_types,
+        data_tf=data_tfs
+    )
+    eval_loader = DataLoader(
+        dataset=eval_set, batch_size=args.batch_size, shuffle=False, drop_last=False, num_workers=args.num_workers
+    )
+    eval_accus = teach_inference(
+        args=args, corruption_types=corruption_types, pans=pans, clsfs=clsfs, data_loader=eval_loader
+    )
+    print({k:round(v, ndigits=4) for k,v in eval_accus.items()})
+    logger.log(data={f'Evaluation/{corruption_type} Accuracy': eval_accus[corruption_type] for corruption_type in corruption_types}, step=step)
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--dataset', type=str, default='SpeechCommandsV2', choices=['SpeechCommandsV2'])
+    ap.add_argument('--adpt_set_path', type=str)
+    ap.add_argument('--eval_set_path', type=str)
+    ap.add_argument('--num_workers', type=int, default=16)
+    ap.add_argument('--output_path', type=str, default='./result')
+    ap.add_argument('--batch_size', type=int, default=64)
+    ap.add_argument('--adpt_wght_pth', type=str)
+    ap.add_argument('--corruption_level', type=str, choices=['L1', 'L2'])
+    ap.add_argument('--elect_weights', type=str)
+    ap.add_argument('--num_of_shft', type=int, default=3, help='maximum number of shifting teachers')
+    ap.add_argument('--fail_coll_lim', type=int, default=3, help='maximum number of fail prediction be choosed in worst list')
+    ap.add_argument('--max_epoch', type=int, default=20)
+    ap.add_argument('--forbid_ls', type=str, default="")
+    ap.add_argument('--unfrz_pos', type=int, default=-1)
+
+    ap.add_argument('--lr', type=float, default=1e-3)
+    ap.add_argument('--lr_cardinality', type=int, default=40)
+    ap.add_argument('--lr_gamma', type=int, default=10)
+    ap.add_argument('--lr_threshold', type=int, default=1)
+    ap.add_argument('--lr_momentum', type=float, default=.9)
+    ap.add_argument('--pan_lr_decay', type=float, default=1.0)
+    ap.add_argument('--clsf_lr_decay', type=float, default=1.0)
+    ap.add_argument('--interval', type=int, default=1, help='interval number')
+
+    ap.add_argument('--wandb', action='store_true')
+    ap.add_argument('--seed', type=int, default=2026, help='random seed')
+
+    args = ap.parse_args()
+    if args.dataset == 'SpeechCommandsV2':
+        args.class_num = 35
+        args.sample_rate = 16000
+    else:
+        raise Exception('No support!')
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.arch = 'PANNs'
+    args.elect_weights = json.loads(args.elect_weights)
+    if not args.forbid_ls.strip():  args.forbid_ls = []
+    else: args.forbid_ls = args.forbid_ls.split(',')
+    args.output_path = os.path.join(args.output_path, args.dataset, args.arch, constants.TEACHER_CONSENSUS)
+    make_unless_exits(args.output_path)
+    torch.backends.cudnn.benchmark = True
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    print_argparse(args)
+    ##########################################
+    wandb_run = wandb.init(
+        project=f'{constants.PROJECT_TITLE}-{constants.TEACHER_CONSENSUS}', 
+        name=f'{constants.architecture_dic[args.arch]}-{constants.dataset_dic[args.dataset]}-{args.corruption_level}', 
+        mode='online' if args.wandb else 'disabled', 
+        config=args, tags=['Audio Classification', 'Teacher Adaptation', args.dataset]
+    )
+
+    print("Initialization...")
+    corruption_types=['WHN', 'ENQ', 'END1', 'END2', 'ENSC', 'PSH', 'TST']
+    teach_pans, teach_clsfs = [], []
+    max_pseudo_accu = 0.
+    optimizers = []
+    loss_fun = CrossEntropyLabelSmooth(num_classes=args.class_num, use_gpu=torch.cuda.is_available())
+    for corruption_type in tqdm(corruption_types):
+        cmeta = CorruptionMeta(type=corruption_type, level=args.corruption_level)
+        teach_pan, teach_clsf = build_model(args=args)
+        load_weight(args=args, panns=teach_pan, clsf=teach_clsf, mode='adaptation', metaInfo=cmeta)
+        teach_pans.append(teach_pan); teach_clsfs.append(teach_clsf)
+        optimizer = build_optimizer(lr=args.lr, auT=teach_pan, auC=teach_clsf, auT_decay=args.pan_lr_decay, auC_decay=args.clsf_lr_decay)
+        optimizers.append(optimizer)
+
+    print("Preparing datasets...")
+    data_tfs = [Components(transforms=[
+        Resample(orig_freq=args.sample_rate, new_freq=constants.pann_sample_rate),
+        AudioPadding(
+            max_length=constants.pann_sample_rate, sample_rate=constants.pann_sample_rate, random_shift=False
+        ),
+        ReduceChannel()
+    ])] * len(corruption_type)
+
+    max_pl_accu = 0.
+    for epoch in range(args.max_epoch+1):
+        print(f'Epoch: {epoch+1}/{args.max_epoch} processing...')
+        teacher_accu_analyzing(
+            args=args, pans=teach_pans, clsfs=teach_clsfs, corruption_types=corruption_types, data_tfs=data_tfs,
+            step=epoch, logger=wandb_run
+        )
+        exit()
+        output_cache, pred_cache, idx_cache, pseudo_accu = pseudo_labeling(
+            args=args, pans=teach_pans, clsfs=teach_clsfs, data_tfs=data_tfs, corruption_types=corruption_types,
+            step=epoch, logger=wandb_run
+        )
+        if max_pl_accu <= pseudo_accu:
+            max_pl_accu = pseudo_accu
+            for i, corruption_type in enumerate(corruption_types):
+                store_weight(
+                    args=args, panns=teach_pans[i], clsf=teach_clsfs[i], mode='adaptation', root_path=args.output_path,
+                    metaInfo=CorruptionMeta(type=corruption_type, level=args.corruption_level)
+                )
+        worst_list, shft_typs = collect_worst_item(
+            args=args, corruption_types=corruption_types, idx_cache=idx_cache, pred_cache=pred_cache, 
+            output_cache=output_cache, step=epoch, logger=wandb_run, feature_label=False
+        )
+        if epoch == args.max_epoch: break
+        print('Adapting...')
+        for teach_pan in teach_pans: teach_pan.train()
+        for teach_clsf in teach_clsfs: teach_clsf.train()
+        for idx, corruption_type in tqdm(enumerate(corruption_types), total=len(corruption_types), position=0):
+            adpt_set = SpeechCommandsV2C(
+                root_path=args.adpt_set_path, corruption_level=args.corruption_level, corruption_type=corruption_types,
+                data_tf=data_tfs
+            )
+            adpt_set = PseudoLabelSet(dataset=adpt_set, pseudo_labels=worst_list[corruption_type], label_position=1)
+            adpt_set = Subset(dataset=adpt_set, id_list=list(worst_list[corruption_type].keys()))
+            adpt_loader = DataLoader(
+                dataset=adpt_set, batch_size=args.batch_size, shuffle=True, drop_last=False, num_workers=args.num_workers
+            )
+            teach_pan, teach_clsf = teach_pans[idx], teach_clsfs[idx]
+            optimizer = optimizers[idx]
+
+            for features, labels in tqdm(adpt_loader, desc=f'{corruption_type}-{args.corruption_level}', position=1, leave=False):
+                if corruption_type not in shft_typs: break
+                if corruption_type in args.forbid_ls: break
+                features, labels = features.to(args.device), labels.to(args.device)
+                if features.shape[0] == 1:
+                    features = features.repeat(4, 1)
+                    labels = labels.repeat(4)
+
+                outputs = teach_clsf(teach_pan(features)['embedding'])
+                clsf_loss = loss_fun(outputs, labels)
+
+                optimizer.zero_grad()
+                clsf_loss.backward()
+                optimizer.step()
+
+            if epoch % args.interval == 0:
+                lr_scheduler(
+                    optimizer=optimizer, epoch=epoch+1, lr_cardinality=args.lr_cardinality,
+                    gamma=args.lr_gammas[corruption_type], threshold=args.lr_threshold, momentum=args.lr_momentum
+                )
+    wandb_run.finish()
+    print('END!')
