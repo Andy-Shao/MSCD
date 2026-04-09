992a8153f7fb868ae6e2c2f952b02638a47ad8b2
Andy-Shao <hj13035952619@126.com>
2026-04-09 13:45:52 +0000

    add analysis for PANNs on SC2-C

---
+++ runs/SpeechCommandsV2/PANNs/analysis/analysis.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+BASE_PATH=${BASE_PATH:-'/root'}
+
+# Student performance analysis
+python -m runs.SpeechCommandsV2.PANNs.analysis.std_anal --dataset 'SpeechCommandsV2' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --output_file 'PAN_SC2-C_L2.csv' --batch_size 32 --corruption_level 'L2' \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
+    --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
