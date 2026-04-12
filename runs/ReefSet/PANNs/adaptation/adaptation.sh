a290a52106ce6b368acf15912abc6c689f57677e
Andy-Shao <hj13035952619@126.com>
2026-04-12 20:19:12 +0000

    optimization

--- runs/ReefSet/PANNs/adaptation/adaptation.sh
+++ runs/ReefSet/PANNs/adaptation/adaptation.sh
@@ -41,5 +41,5 @@ python -m runs.ReefSet.PANNs.adaptation.ttda --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
     --batch_size 70 --corruption_level 'L2' --corruption_type 'PSH' --lr 1e-4 --max_epoch 20 \
-    --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.1 \
+    --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --gent_q 2.1 --mse_rate 0.1 \
     --orig_wght_pth './result/ReefSet/PANNs/train' --wandb
