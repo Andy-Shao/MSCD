424c54c2edf86a07475852a36c81dd3b04f79450
Andy-Shao <hj13035952619@126.com>
2026-04-17 12:44:22 +0000

    add TTDA for PANNs on US8-C

---
+++ runs/UrbanSound8K/PANNs/adaptation/adaptation.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+# Teacher Adaptation
+python -m runs.UrbanSound8K.PANNs.adaptation.ttda --dataset 'UrbanSound8K' \
+    --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
+    --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
+    --corruption_type 'WHN' --corruption_level 'L2' --batch_size 70 --max_epoch 20 --lr 1e-4 \
+    --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.0 \
+    --orig_wght_pth './result/UrbanSound8K/PANNs/train' --wandb
