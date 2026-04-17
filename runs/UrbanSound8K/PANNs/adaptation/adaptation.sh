7d87b2ee19614dd8e502948591ffab7f938914da
Andy-Shao <hj13035952619@126.com>
2026-04-17 13:41:07 +0000

    optimization

--- runs/UrbanSound8K/PANNs/adaptation/adaptation.sh
+++ runs/UrbanSound8K/PANNs/adaptation/adaptation.sh
@@ -7,4 +7,5 @@ python -m runs.UrbanSound8K.PANNs.adaptation.ttda --dataset 'UrbanSound8K' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
     --corruption_type 'WHN' --corruption_level 'L2' --batch_size 70 --max_epoch 25 --lr 1e-4 \
     --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.0 \
+    --freeze_pan \
     --orig_wght_pth './result/UrbanSound8K/PANNs/train' --wandb
