dc36c704c6a422d2e6e008870eafc70288799026
Andy-Shao <hj13035952619@126.com>
2026-04-17 11:10:05 +0000

    optimization

--- runs/UrbanSound8K/PANNs/train/train.sh
+++ runs/UrbanSound8K/PANNs/train/train.sh
@@ -3,4 +3,4 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m runs.UrbanSound8K.PANNs.train.train --dataset 'UrbanSound8K' \
     --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
-    --batch_size 33 --lr 1e-3 --max_epoch 30 --wandb 
+    --batch_size 33 --lr 5e-4 --max_epoch 30 --wandb 
