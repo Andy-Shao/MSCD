222aaeb55790eac0fdd5b75d0625faa084323e07
Andy-Shao <hj13035952619@126.com>
2026-04-18 11:58:36 +0000

    add kd

--- runs/UrbanSound8K/PANNs/adaptation/adaptation.sh
+++ runs/UrbanSound8K/PANNs/adaptation/adaptation.sh
@@ -32,10 +32,20 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --orig_wght_pth './result/UrbanSound8K/PANNs/train' --wandb
 
 # Teacher Consensus
-python -m runs.UrbanSound8K.PANNs.adaptation.teach_cons --dataset 'UrbanSound8K' \
+# python -m runs.UrbanSound8K.PANNs.adaptation.teach_cons --dataset 'UrbanSound8K' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
+#     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
+#     --corruption_level 'L2' --batch_size 33 --max_epoch 10 --lr 1e-5 \
+#     --num_of_shft 1 --fail_coll_lim 2 --lr_gamma 30 \
+#     --elect_weights '{"WHN":1.3, "ENSC":0.8, "PSH":1.1, "TST":1.5}' \
+#     --adpt_wght_pth './result/UrbanSound8K/PANNs/TTDA' --wandb
+
+# Knowledge Distillation
+python -m runs.UrbanSound8K.PANNs.adaptation.kd --dataset 'UrbanSound8K' \
     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
-    --corruption_level 'L2' --batch_size 33 --max_epoch 10 --lr 1e-5 \
-    --num_of_shft 1 --fail_coll_lim 2 --lr_gamma 30 \
+    --corruption_level 'L2' --batch_size 33 --max_epoch 20 --lr 1e-4 \
+    --pseudo_threshold 3.62 --ctr_rt 0.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
     --elect_weights '{"WHN":1.3, "ENSC":0.8, "PSH":1.1, "TST":1.5}' \
-    --adpt_wght_pth './result/UrbanSound8K/PANNs/TTDA' --wandb
+    --orig_wght_pth './result/UrbanSound8K/PANNs/train' \
+    --adpt_wght_pth './result/UrbanSound8K/PANNs/Teach-Cons' --wandb
