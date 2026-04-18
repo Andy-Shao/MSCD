c9a559c7e0b7a05a52e146583f2b00fa2cfb5c5f
Andy-Shao <hj13035952619@126.com>
2026-04-18 15:25:34 +0000

    clean code

--- runs/SpeechCommandsV2/PANNs/analysis/silhouette_anal.py
+++ runs/SpeechCommandsV2/PANNs/analysis/silhouette_anal.py
@@ -112,5 +112,5 @@ if __name__ == '__main__':
     # load_weight(args=args, panns=std_pan, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION)
     # adpt_scr = silhouette_inference(args=args, pan=std_pan, data_loader=eval_loader, clsf=std_clsf, mode='logits')
     # records.loc[len(records)] = [args.arch, 'logits', org_scr, adpt_scr, (adpt_scr - org_scr)/abs(org_scr)]
-    # records.to_csv(os.path.join(args.output_path, args.output_file))
+    records.to_csv(os.path.join(args.output_path, args.output_file))
     print('END!')
