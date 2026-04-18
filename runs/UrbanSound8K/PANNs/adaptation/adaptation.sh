82dc4f7ef483ecb28bfecc0553112232d8d9a765
Andy-Shao <hj13035952619@126.com>
2026-04-18 13:15:03 +0000

    optimization

--- runs/UrbanSound8K/PANNs/adaptation/adaptation.sh
+++ runs/UrbanSound8K/PANNs/adaptation/adaptation.sh
@@ -45,7 +45,7 @@ python -m runs.UrbanSound8K.PANNs.adaptation.kd --dataset 'UrbanSound8K' \
     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
     --corruption_level 'L2' --batch_size 33 --max_epoch 20 --lr 1e-4 \
-    --pseudo_threshold 3.62 --ctr_rt 0.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
+    --pseudo_threshold 3.62 --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
     --elect_weights '{"WHN":1.3, "ENSC":0.8, "PSH":1.1, "TST":1.5}' \
     --orig_wght_pth './result/UrbanSound8K/PANNs/train' \
     --adpt_wght_pth './result/UrbanSound8K/PANNs/Teach-Cons' --wandb
