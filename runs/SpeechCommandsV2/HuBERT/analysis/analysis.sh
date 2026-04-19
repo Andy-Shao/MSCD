fb28f1932bf3a760e18af233692eb90e50a60087
Andy-Shao <hj13035952619@126.com>
2026-04-19 12:16:28 +0000

    adjust analysis

--- runs/SpeechCommandsV2/HuBERT/analysis/analysis.sh
+++ runs/SpeechCommandsV2/HuBERT/analysis/analysis.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# Student Analysis
 python -m runs.SpeechCommandsV2.HuBERT.analysis.std_anal --dataset 'SpeechCommandsV2' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
     --output_file 'HuB_SC2-C_L2.csv' --batch_size 32 --corruption_level 'L2' --model_level 'base' \
+    --orig_wght_pth $BASE_PATH'/result/SpeechCommandsV2/HuBERT/train' \
     --std_adpt_wght_pth './result/SpeechCommandsV2/HuBERT/KD'
--- runs/SpeechCommandsV2/HuBERT/analysis/std_anal.py
+++ runs/SpeechCommandsV2/HuBERT/analysis/std_anal.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     ap.add_argument('--output_path', type=str, default='./result')
     ap.add_argument('--output_file', type=str, default='result.csv')
     ap.add_argument('--batch_size', type=int, default=64)
+    ap.add_argument('--orig_wght_pth', type=str)
     ap.add_argument('--std_adpt_wght_pth', type=str)
     ap.add_argument('--corruption_level', type=str, choices=['L1', 'L2'])
 
@@ -51,11 +52,10 @@ if __name__ == '__main__':
     ##########################################
     corruption_types=['WHN', 'ENQ', 'END1', 'END2', 'ENSC', 'PSH', 'TST']
     data_tfs = [ReduceChannel()] * len(corruption_types)
-    records = pd.DataFrame(columns=['Model', 'Num of Params', 'Corruption', 'Accuracy'])
+    records = pd.DataFrame(columns=['Model', 'Num of Params', 'Corruption', 'Befor Adaptation', 'After Adaptation'])
 
     print("Initialization...")
-    std_hub, std_clsf = build_model(args=args)
-    load_weight(args=args, hubert=std_hub, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION)
+    std_hub, std_clsf = build_model(args=args, pre_weight=False)
     aut_pth, clsf_pth = __cal_model_path__(args=args, mode=constants.STUDENT_ADAPTATION, root_path=args.output_path)
     aut_pth = aut_pth.replace('.pt', '.txt')
     clsf_pth = clsf_pth.replace('.pt', '.txt')
@@ -71,11 +71,23 @@ if __name__ == '__main__':
     )
 
     print('Analyzing...')
+    print('Before Adaptation Analysis...')
+    load_weight(args=args, hubert=std_hub, clsf=std_clsf, mode='origin')
+    org_glb_accu, org_lcl_accus = mlt_inference(
+        args=args, corruption_types=corruption_types, hub=std_hub, clsf=std_clsf, data_loader=eval_loader
+    )
+
+    print('After Adaptation Analysis...')
+    load_weight(args=args, hubert=std_hub, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION)
     global_accu, local_accus = mlt_inference(
         args=args, corruption_types=corruption_types, hub=std_hub, clsf=std_clsf, data_loader=eval_loader
     )
+
     for corruption_type, local_accu in local_accus.items():
-        records.loc[len(records)] = [args.arch, param_num, f'{corruption_type}-{args.corruption_level}', local_accu]
-    records.loc[len(records)] = [args.arch, param_num, 'Global', global_accu]
+        records.loc[len(records)] = [
+            args.arch, param_num, f'{corruption_type}-{args.corruption_level}', org_lcl_accus[corruption_type],
+            local_accu
+        ]
+    records.loc[len(records)] = [args.arch, param_num, 'Global', org_glb_accu, global_accu]
     records.to_csv(os.path.join(args.output_path, args.output_file))
     print('END!')
