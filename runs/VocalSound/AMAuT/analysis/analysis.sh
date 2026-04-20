04b101afde17987d31890a6dd59a5eca0748b1e1
Andy-Shao <hj13035952619@126.com>
2026-04-20 09:09:16 +0000

    add silhouette

--- runs/VocalSound/AMAuT/analysis/analysis.sh
+++ runs/VocalSound/AMAuT/analysis/analysis.sh
@@ -2,8 +2,15 @@
 BASE_PATH=${BASE_PATH:-'/root'}
 
 # Student Model Analysis
-python -m runs.VocalSound.AMAuT.analysis.std_anal --dataset 'VocalSound' \
+# python -m runs.VocalSound.AMAuT.analysis.std_anal --dataset 'VocalSound' \
+#     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+#     --output_file 'AuT_VS-C_L2.csv' --corruption_level 'L2' --batch_size 32 \
+#     --orig_wght_pth $BASE_PATH'/result/VocalSound/AMAuT/train' \
+#     --std_adpt_wght_pth './result/VocalSound/AMAuT/KD'
+
+# Silhouette Analysis
+python -m runs.VocalSound.AMAuT.analysis.silhouette_anal --dataset 'VocalSound' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-    --output_file 'AuT_VS-C_L2.csv' --corruption_level 'L2' --batch_size 32 \
+    --output_file 'silhouette_AuT_VS-C_L2.csv' --corruption_level 'L2' --batch_size 32 \
     --orig_wght_pth $BASE_PATH'/result/VocalSound/AMAuT/train' \
     --std_adpt_wght_pth './result/VocalSound/AMAuT/KD'
