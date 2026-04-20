2738913ec1c9d49d96daebf3db27c03f061e8510
Andy-Shao <hj13035952619@126.com>
2026-04-20 09:34:02 +0000

    upgrade analysis

--- runs/VocalSound/HuBERT/analysis/analysis.sh
+++ runs/VocalSound/HuBERT/analysis/analysis.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# Student Analysis
 python -m runs.VocalSound.HuBERT.analysis.std_anal --dataset 'VocalSound' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
     --output_file 'HuB_VS-C_L2.csv' --batch_size 32 --corruption_level 'L2' --model_level 'base' \
+    --orig_wght_pth $BASE_PATH'/result/VocalSound/HuBERT/train' \
     --std_adpt_wght_pth './result/VocalSound/HuBERT/KD'
