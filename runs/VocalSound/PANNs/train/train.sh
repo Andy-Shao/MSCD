c827161e886cca5f0132c90afee57f4ed5afc0e9
Andy-Shao <hj13035952619@126.com>
2026-04-21 09:35:06 +0000

    optimization

--- runs/VocalSound/PANNs/train/train.sh
+++ runs/VocalSound/PANNs/train/train.sh
@@ -3,4 +3,4 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m runs.VocalSound.PANNs.train.train --dataset 'VocalSound' \
     --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
-    --batch_size 32 --lr 1e-4 --max_epoch 20 --wandb
+    --batch_size 32 --lr 1e-4 --max_epoch 30 --wandb
