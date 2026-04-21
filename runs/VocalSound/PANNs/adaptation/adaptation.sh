b28e86f2a5ed51d8ed89ef19ec763f982fe2a4e5
Andy-Shao <hj13035952619@126.com>
2026-04-21 13:17:36 +0000

    add TTDA on VS-C

---
+++ runs/VocalSound/PANNs/adaptation/adaptation.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+# Teacher Adaptation
+python -m runs.VocalSound.PANNs.adaptation.ttda --dataset 'VocalSound' \
+    --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+    --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+    --corruption_type 'WHN' --corruption_level 'L2' --batch_size 32 --max_epoch 10 \
+    --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.0 \
+    --orig_wght_pth './result/VocalSound/PANNs/train' --freeze_pan --wandb
