49aff5af96671cbbd2eedf993cd8c27e0edf8ae8
Andy-Shao <hj13035952619@126.com>
2026-04-26 10:46:55 +0000

    optimization

--- runs/VocalSound/PANNs/adaptation/adaptation.sh
+++ runs/VocalSound/PANNs/adaptation/adaptation.sh
@@ -21,7 +21,7 @@ python -m runs.VocalSound.PANNs.adaptation.ttda --dataset 'VocalSound' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
     --corruption_type 'END1' --corruption_level 'L2' --batch_size 70 --max_epoch 10 \
     --lr 1e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.1 --gent_q 1.6 --mse_rate 0.1 \
-    --pan_lr_decay 0.55 --lr_momentum 0.70 \
+    --pan_lr_decay 1.0 --lr_momentum 0.75 \
     --orig_wght_pth './result/VocalSound/PANNs/train' --freeze_pan --wandb
 
 # python -m runs.VocalSound.PANNs.adaptation.ttda --dataset 'VocalSound' \
