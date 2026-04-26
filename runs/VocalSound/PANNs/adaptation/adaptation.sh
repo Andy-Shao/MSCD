476772911946a962480dbfd013a5b67a9b0ab540
Andy-Shao <hj13035952619@126.com>
2026-04-26 18:38:43 +0000

    optimization

--- runs/VocalSound/PANNs/adaptation/adaptation.sh
+++ runs/VocalSound/PANNs/adaptation/adaptation.sh
@@ -53,21 +53,21 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --orig_wght_pth './result/VocalSound/PANNs/train' --freeze_pan --max_mode --wandb
 
 # Teacher Consensus
-python -m runs.VocalSound.PANNs.adaptation.teach_cons --dataset 'VocalSound' \
-    --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
-    --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-    --corruption_level 'L2' --batch_size 32 --max_epoch 14 --num_of_shft 3 --fail_coll_lim 4 \
-    --elect_weights '{"WHN":1.5, "ENQ":1.5, "END1":2.0, "END2":2.0, "ENSC":1.0, "PSH":0.8, "TST":0.8}' \
-    --lrs '{"WHN":1e-4, "ENQ":1e-4, "END1":1e-4, "END2":1e-4, "ENSC":1e-4, "PSH":3e-4, "TST":1e-4}' \
-    --adpt_wght_pth './result/VocalSound/PANNs/TTDA' --forbid_ls 'END1' --wandb
-
-# Knowledge Distillation
-# python -m runs.VocalSound.PANNs.adaptation.kd --dataset 'VocalSound' \
+# python -m runs.VocalSound.PANNs.adaptation.teach_cons --dataset 'VocalSound' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
 #     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-#     --corruption_level 'L2' --batch_size 32 --max_epoch 20 --lr 1e-4 --pseudo_threshold 6.77 \
-#     --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
-#     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":2.0, "END2":2.0, "ENSC":1.0, "PSH":1.0, "TST":0.8}' \
-#     --orig_wght_pth './result/VocalSound/PANNs/train' \
-#     --adpt_wght_pth './result/VocalSound/PANNs/Teach-Cons' \
-#     --max_mode --wandb
+#     --corruption_level 'L2' --batch_size 32 --max_epoch 14 --num_of_shft 3 --fail_coll_lim 4 \
+#     --elect_weights '{"WHN":1.5, "ENQ":1.5, "END1":2.0, "END2":2.0, "ENSC":1.0, "PSH":0.8, "TST":0.8}' \
+#     --lrs '{"WHN":1e-4, "ENQ":1e-4, "END1":1e-4, "END2":1e-4, "ENSC":1e-4, "PSH":3e-4, "TST":1e-4}' \
+#     --adpt_wght_pth './result/VocalSound/PANNs/TTDA' --forbid_ls 'END1' --wandb
+
+# Knowledge Distillation
+python -m runs.VocalSound.PANNs.adaptation.kd --dataset 'VocalSound' \
+    --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+    --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+    --corruption_level 'L2' --batch_size 32 --max_epoch 20 --lr 1e-4 --pseudo_threshold 6.77 \
+    --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
+    --elect_weights '{"WHN":1.5, "ENQ":1.5, "END1":2.0, "END2":2.0, "ENSC":1.0, "PSH":0.8, "TST":0.8}' \
+    --orig_wght_pth './result/VocalSound/PANNs/train' \
+    --adpt_wght_pth './result/VocalSound/PANNs/Teach-Cons' \
+    --max_mode --wandb
