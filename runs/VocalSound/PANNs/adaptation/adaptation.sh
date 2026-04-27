dcc4149a691b4bef7c7ca452dddd88c7d309a8e0
Andy-Shao <hj13035952619@126.com>
2026-04-27 10:12:35 +0000

    optimization

--- runs/VocalSound/PANNs/adaptation/adaptation.sh
+++ runs/VocalSound/PANNs/adaptation/adaptation.sh
@@ -65,7 +65,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.VocalSound.PANNs.adaptation.kd --dataset 'VocalSound' \
     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-    --corruption_level 'L2' --batch_size 32 --max_epoch 20 --lr 1e-4 --pseudo_threshold 6.77 \
+    --corruption_level 'L2' --batch_size 32 --max_epoch 20 --lr 1e-4 --pseudo_threshold 7.38 \
     --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
     --elect_weights '{"WHN":1.5, "ENQ":1.5, "END1":2.0, "END2":2.0, "ENSC":1.0, "PSH":0.8, "TST":0.8}' \
     --orig_wght_pth './result/VocalSound/PANNs/train' \
