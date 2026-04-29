92d06a165075b34fe953115e2cc4ff236374c5eb
Andy-Shao <hj13035952619@126.com>
2026-04-29 15:02:40 +0000

    optimization

--- runs/SpeechCommandsV2/AMAuT/adaptation/adaptatoin.sh
+++ runs/SpeechCommandsV2/AMAuT/adaptation/adaptatoin.sh
@@ -32,7 +32,7 @@ python -m runs.SpeechCommandsV2.AMAuT.adaptation.kd --dataset 'SpeechCommandsV2'
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' --batch_size 32 \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
     --corruption_level 'L1' --max_epoch 20 --lr '1e-4' --pseudo_threshold 7.69 \
-    --lr_threshold 10 --ctr_rt 0.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
+    --lr_threshold 10 --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.5, "END2":1.5, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
     --orig_wght_pth $BASE_PATH'/result/SpeechCommandsV2/AMAuT/train' \
     --adpt_wght_pth './result/SpeechCommandsV2/AMAuT/Teach-Cons' --wandb
