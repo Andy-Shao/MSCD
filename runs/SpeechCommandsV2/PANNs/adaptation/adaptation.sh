ba22e026e011a4a7056e2b90e1950962ce1eb705
Andy-Shao <hj13035952619@126.com>
2026-05-02 08:44:48 +0000

    add ENQ-L1 teacher adaptation

--- runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
@@ -10,13 +10,13 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.1 \
 #     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
 
-python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
-    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
-    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --corruption_type 'WHN' --corruption_level 'L1' --batch_size 70 --max_epoch 25 \
-    --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --mse_rate 0.1 \
-    --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 3.1 \
-    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
+# python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+#     --corruption_type 'WHN' --corruption_level 'L1' --batch_size 70 --max_epoch 25 \
+#     --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --mse_rate 0.1 \
+#     --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 3.1 \
+#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
 
 # python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
@@ -25,6 +25,14 @@ python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV
 #     --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.1 --mse_rate 0.1 \
 #     --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.6 \
 #     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
+
+python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --corruption_type 'ENQ' --corruption_level 'L1' --batch_size 70 --max_epoch 20 \
+    --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --mse_rate 0.0 \
+    --lr_momentum 0.9 --pan_lr_decay 1.0 --gent_q 1.6 \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
 
 # python -m runs.SpeechCommandsV2.PANNs.adaptation.ttda --dataset 'SpeechCommandsV2' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
