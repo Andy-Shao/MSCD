bf4dbc7159166d17b6ba8ec432a967b60f0d202a
Andy-Shao <hj1303592619@126.com>
2026-05-04 14:15:29 +0000

    add KD

--- runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
@@ -125,14 +125,14 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --lr_cardinalities '{"WHN":60, "ENQ":60, "END1":60, "END2":60, "ENSC":60, "PSH":60, "TST":60}' \
 #     --adpt_wght_pth './result/SpeechCommandsV2/PANNs/TTDA' --max_mode --wandb
 
-python -m runs.SpeechCommandsV2.PANNs.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
-    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
-    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --batch_size 32 --corruption_level 'L1' --num_of_shft 4 --fail_coll_lim 4 --max_epoch 120 --lr_threshold 10 \
-    --elect_weights '{"WHN":0.8, "ENQ":1.0, "END1":1.65, "END2":1.65, "ENSC":0.9, "PSH":1.0, "TST":2.1}' \
-    --lrs '{"WHN":1e-5, "ENQ":3e-5, "END1":2.5e-6, "END2":2.5e-6, "ENSC":3e-5, "PSH":7e-5, "TST":1e-4}' \
-    --lr_cardinalities '{"WHN":50, "ENQ":40, "END1":40, "END2":40, "ENSC":40, "PSH":60, "TST":40}' \
-    --adpt_wght_pth './result/SpeechCommandsV2/PANNs/TTDA' --wandb
+# python -m runs.SpeechCommandsV2.PANNs.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+#     --batch_size 32 --corruption_level 'L1' --num_of_shft 4 --fail_coll_lim 4 --max_epoch 120 --lr_threshold 10 \
+#     --elect_weights '{"WHN":0.8, "ENQ":1.0, "END1":1.65, "END2":1.65, "ENSC":0.9, "PSH":1.0, "TST":2.1}' \
+#     --lrs '{"WHN":1e-5, "ENQ":3e-5, "END1":2.5e-6, "END2":2.5e-6, "ENSC":3e-5, "PSH":7e-5, "TST":1e-4}' \
+#     --lr_cardinalities '{"WHN":50, "ENQ":40, "END1":40, "END2":40, "ENSC":40, "PSH":60, "TST":40}' \
+#     --adpt_wght_pth './result/SpeechCommandsV2/PANNs/TTDA' --wandb
 
 # Knowledge Distillation
 # python -m runs.SpeechCommandsV2.PANNs.adaptation.kd --dataset 'SpeechCommandsV2' \
@@ -143,3 +143,12 @@ python -m runs.SpeechCommandsV2.PANNs.adaptation.teach_cons --dataset 'SpeechCom
 #     --lr 1e-4 --pseudo_threshold 6.0 --lr_cardinality 80 --lr_threshold 20 \
 #     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
 #     --adpt_wght_pth './result/SpeechCommandsV2/PANNs/Teach-Cons' --wandb
+
+python -m runs.SpeechCommandsV2.PANNs.adaptation.kd --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --batch_size 32 --corruption_level 'L1' --max_epoch 20 --ctr_rt 0.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
+    --elect_weights '{"WHN":0.8, "ENQ":1.0, "END1":1.65, "END2":1.65, "ENSC":0.9, "PSH":1.0, "TST":2.1}' \
+    --lr 1e-4 --pseudo_threshold 7.0 --lr_cardinality 40 --lr_threshold 1 \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
+    --adpt_wght_pth './result/SpeechCommandsV2/PANNs/Teach-Cons' --wandb
--- runs/SpeechCommandsV2/PANNs/adaptation/kd.py
+++ runs/SpeechCommandsV2/PANNs/adaptation/kd.py
@@ -230,7 +230,8 @@ if __name__ == '__main__':
         if max_accu <= accu:
             max_accu = accu
             store_weight(
-                args=args, panns=std_pan, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION, root_path=args.output_path
+                args=args, panns=std_pan, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION, root_path=args.output_path,
+                metaInfo=CorruptionMeta(type=None, level=args.corruption_level)
             )
         if epoch >= args.max_epoch: break
         print('Adaptating...')
