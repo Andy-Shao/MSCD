1b8ad77fe43d684dfadb7a78df7b88dcfefa69d0
Andy-Shao <hj1303592619@126.com>
2026-05-04 14:11:53 +0000

    optimization

--- runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
@@ -128,7 +128,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.SpeechCommandsV2.PANNs.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --batch_size 32 --corruption_level 'L1' --num_of_shft 4 --fail_coll_lim 4 --max_epoch 80 --lr_threshold 10 \
+    --batch_size 32 --corruption_level 'L1' --num_of_shft 4 --fail_coll_lim 4 --max_epoch 120 --lr_threshold 10 \
     --elect_weights '{"WHN":0.8, "ENQ":1.0, "END1":1.65, "END2":1.65, "ENSC":0.9, "PSH":1.0, "TST":2.1}' \
     --lrs '{"WHN":1e-5, "ENQ":3e-5, "END1":2.5e-6, "END2":2.5e-6, "ENSC":3e-5, "PSH":7e-5, "TST":1e-4}' \
     --lr_cardinalities '{"WHN":50, "ENQ":40, "END1":40, "END2":40, "ENSC":40, "PSH":60, "TST":40}' \
