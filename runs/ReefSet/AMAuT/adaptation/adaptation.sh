ee49f399a3a6129f4b340c4ef35a199e8fa20e06
Andy-Shao <hj130359526119@126.com>
2026-05-07 12:28:20 +0000

    add RS-C L1 on AuT

--- runs/ReefSet/AMAuT/adaptation/adaptation.sh
+++ runs/ReefSet/AMAuT/adaptation/adaptation.sh
@@ -10,12 +10,20 @@ BASE_PATH=${BASE_PATH:-'/root'}
 #     --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.2, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
 #     --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' --wandb
 
-# Knowledge Distillation
-python -m runs.ReefSet.AMAuT.adaptation.kd --dataset 'ReefSet' \
+python -m runs.ReefSet.AMAuT.adaptation.teach_cons --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr 1e-4 --pseudo_threshold 7.5 \
-    --ctr_rt 0.5 --ctr_dist 'sq_l2' --aut_lr_decay 0.55 --lr_momentum 0.75 \
-    --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.2, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
-    --adpt_wght_pth './result/ReefSet/AMAuT/Teach-Cons' \
-    --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' --wandb
+    --batch_size 32 --corruption_level 'L1' --max_epoch 20 --lr 1e-4 --num_of_shft 3 --fail_coll_lim 3 \
+    --forbid_ls 'TST' \
+    --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.1, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":4.0}' \
+    --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' --wandb
+
+# Knowledge Distillation
+# python -m runs.ReefSet.AMAuT.adaptation.kd --dataset 'ReefSet' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+#     --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr 1e-4 --pseudo_threshold 7.5 \
+#     --ctr_rt 0.5 --ctr_dist 'sq_l2' --aut_lr_decay 0.55 --lr_momentum 0.75 \
+#     --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.2, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
+#     --adpt_wght_pth './result/ReefSet/AMAuT/Teach-Cons' \
+#     --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' --wandb
