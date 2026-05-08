71dc7fe4b710e5075937dba575e608e7f63d47da
Andy-Shao <hj13035952619@126.com>
2026-05-08 10:56:33 +0000

    add KD

--- runs/ReefSet/AMAuT/adaptation/adaptation.sh
+++ runs/ReefSet/AMAuT/adaptation/adaptation.sh
@@ -10,12 +10,12 @@ BASE_PATH=${BASE_PATH:-'/root'}
 #     --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.2, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
 #     --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' --max_mode --wandb
 
-python -m runs.ReefSet.AMAuT.adaptation.teach_cons --dataset 'ReefSet' \
-    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
-    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L1' --max_epoch 10 --lr 1e-4 --num_of_shft 6 --fail_coll_lim 3 \
-    --elect_weights '{"WHN":2.0, "ENQ":1.0, "END1":2.0, "END2":1.0, "ENSC":1.0, "PSH":2.0, "TST":4.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' --wandb
+# python -m runs.ReefSet.AMAuT.adaptation.teach_cons --dataset 'ReefSet' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+#     --batch_size 32 --corruption_level 'L1' --max_epoch 10 --lr 1e-4 --num_of_shft 6 --fail_coll_lim 3 \
+#     --elect_weights '{"WHN":2.0, "ENQ":1.0, "END1":2.0, "END2":1.0, "ENSC":1.0, "PSH":2.0, "TST":4.0}' \
+#     --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' --wandb
 
 # Knowledge Distillation
 # python -m runs.ReefSet.AMAuT.adaptation.kd --dataset 'ReefSet' \
@@ -26,3 +26,12 @@ python -m runs.ReefSet.AMAuT.adaptation.teach_cons --dataset 'ReefSet' \
 #     --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":1.2, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
 #     --adpt_wght_pth './result/ReefSet/AMAuT/Teach-Cons' \
 #     --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' --wandb
+
+python -m runs.ReefSet.AMAuT.adaptation.kd --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --batch_size 32 --corruption_level 'L1' --max_epoch 20 --lr 1e-4 --pseudo_threshold 7.69 \
+    --ctr_rt 0.0 --ctr_dist 'sq_l2' --ctr_T 1.0 \
+    --elect_weights '{"WHN":2.0, "ENQ":1.0, "END1":2.0, "END2":1.0, "ENSC":1.0, "PSH":2.0, "TST":4.0}' \
+    --adpt_wght_pth './result/ReefSet/AMAuT/Teach-Cons' \
+    --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' --wandb
