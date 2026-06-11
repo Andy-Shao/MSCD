15c5dab51265658bda541092166b2e9326b778ec
Andy-Shao <hj13035952619@126.com>
2026-06-11 13:49:32 +0000

    optimization

--- runs/SpeechCommandsV2/AMAuT/adaptation/adaptatoin.sh
+++ runs/SpeechCommandsV2/AMAuT/adaptation/adaptatoin.sh
@@ -2,21 +2,21 @@
 BASE_PATH=${BASE_PATH:-'/root'}
 
 # Teacher Consensus
-python -m runs.SpeechCommandsV2.AMAuT.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
-    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' --batch_size 32 \
-    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --corruption_level 'L2' --max_epoch 30 --lr '1e-4' --num_of_shft 5 --lr_gamma 10 \
-    --aut_lr_decay 0.55 --forbid_ls 'TST' --fail_coll_lim 6 --unfrz_pos 22 \
-    --lr_threshold 10 --lr_cardinality 60 \
-    --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.0, "END2":1.0, "ENSC":1.0, "PSH":1.0, "TST":2.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/SpeechCommandsV2/AMAuT/TTDA' --wandb
-
 # python -m runs.SpeechCommandsV2.AMAuT.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' --batch_size 32 \
 #     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-#     --corruption_level 'L1' --max_epoch 30 --lr 1e-4 --num_of_shft 3 --fail_coll_lim 3 \
-#     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.5, "END2":1.5, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
+#     --corruption_level 'L2' --max_epoch 30 --lr '1e-4' --num_of_shft 5 --lr_gamma 10 \
+#     --aut_lr_decay 0.55 --forbid_ls 'TST' --fail_coll_lim 6 --unfrz_pos 22 \
+#     --lr_threshold 10 --lr_cardinality 60 \
+#     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.0, "END2":1.0, "ENSC":1.0, "PSH":1.0, "TST":2.0}' \
 #     --adpt_wght_pth $BASE_PATH'/result/SpeechCommandsV2/AMAuT/TTDA' --wandb
+
+python -m runs.SpeechCommandsV2.AMAuT.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' --batch_size 32 \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --corruption_level 'L1' --max_epoch 30 --lr 1e-4 --num_of_shft 3 --fail_coll_lim 3 \
+    --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.5, "END2":1.5, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
+    --adpt_wght_pth $BASE_PATH'/result/SpeechCommandsV2/AMAuT/TTDA' --wandb
 
 # Knowledge distillation
 # python -m runs.SpeechCommandsV2.AMAuT.adaptation.kd --dataset 'SpeechCommandsV2' \
