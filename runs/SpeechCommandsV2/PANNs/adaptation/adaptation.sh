4d5759b95a9fa9ee790b613ff5b81376788d2e09
Andy-Shao <hj13035952619@126.com>
2026-06-13 15:30:58 +0000

    optimization

--- runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
+++ runs/SpeechCommandsV2/PANNs/adaptation/adaptation.sh
@@ -12,24 +12,24 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --lr_cardinalities '{"WHN":60, "ENQ":60, "END1":60, "END2":60, "ENSC":60, "PSH":60, "TST":60}' \
 #     --adpt_wght_pth $BASE_PATH'/result/SpeechCommandsV2/PANNs/TTDA' --wandb
 
-python -m runs.SpeechCommandsV2.PANNs.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
-    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
-    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --batch_size 32 --corruption_level 'L1' --num_of_shft 4 --fail_coll_lim 4 --max_epoch 60 --lr_threshold 10 \
-    --elect_weights '{"WHN":0.8, "ENQ":1.0, "END1":1.65, "END2":1.65, "ENSC":0.9, "PSH":1.0, "TST":2.1}' \
-    --lrs '{"WHN":1e-5, "ENQ":1e-5, "END1":1e-6, "END2":1e-6, "ENSC":1e-5, "PSH":7e-5, "TST":1e-4}' \
-    --lr_cardinalities '{"WHN":50, "ENQ":40, "END1":40, "END2":40, "ENSC":40, "PSH":60, "TST":40}' \
-    --adpt_wght_pth $BASE_PATH'/result/SpeechCommandsV2/PANNs/TTDA' --max_mode --wandb
-
-# Knowledge Distillation
-# python -m runs.SpeechCommandsV2.PANNs.adaptation.kd --dataset 'SpeechCommandsV2' \
+# python -m runs.SpeechCommandsV2.PANNs.adaptation.teach_cons --dataset 'SpeechCommandsV2' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
 #     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-#     --batch_size 32 --corruption_level 'L2' --max_epoch 100 --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 2.0 \
-#     --elect_weights '{"WHN":0.8, "ENQ":0.8, "END1":1.5, "END2":1.5, "ENSC":0.9, "PSH":0.8, "TST":1.5}' \
-#     --lr 1e-4 --pseudo_threshold 6.0 --lr_cardinality 80 --lr_threshold 20 \
-#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
-#     --adpt_wght_pth './result/SpeechCommandsV2/PANNs/Teach-Cons' --wandb
+#     --batch_size 32 --corruption_level 'L1' --num_of_shft 4 --fail_coll_lim 4 --max_epoch 60 --lr_threshold 10 \
+#     --elect_weights '{"WHN":0.8, "ENQ":1.0, "END1":1.65, "END2":1.65, "ENSC":0.9, "PSH":1.0, "TST":2.1}' \
+#     --lrs '{"WHN":1e-5, "ENQ":1e-5, "END1":1e-6, "END2":1e-6, "ENSC":1e-5, "PSH":7e-5, "TST":1e-4}' \
+#     --lr_cardinalities '{"WHN":50, "ENQ":40, "END1":40, "END2":40, "ENSC":40, "PSH":60, "TST":40}' \
+#     --adpt_wght_pth $BASE_PATH'/result/SpeechCommandsV2/PANNs/TTDA' --max_mode --wandb
+
+# Knowledge Distillation
+python -m runs.SpeechCommandsV2.PANNs.adaptation.kd --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --batch_size 32 --corruption_level 'L2' --max_epoch 100 --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 2.0 \
+    --elect_weights '{"WHN":0.8, "ENQ":0.8, "END1":1.5, "END2":1.5, "ENSC":0.9, "PSH":0.8, "TST":1.5}' \
+    --lr 1e-4 --pseudo_threshold 6.0 --lr_cardinality 80 --lr_threshold 20 \
+    --orig_wght_pth $BASE_PATH'/result/SpeechCommandsV2/PANNs/train' \
+    --adpt_wght_pth './result/SpeechCommandsV2/PANNs/Teach-Cons' --wandb
 
 # python -m runs.SpeechCommandsV2.PANNs.adaptation.kd --dataset 'SpeechCommandsV2' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
