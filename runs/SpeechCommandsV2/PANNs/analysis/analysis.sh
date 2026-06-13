4ae1afc8a086ef55c61009840a1ac0e519104475
Andy-Shao <hj13035952619@126.com>
2026-06-13 22:13:48 +0000

    add analysis

--- runs/SpeechCommandsV2/PANNs/analysis/analysis.sh
+++ runs/SpeechCommandsV2/PANNs/analysis/analysis.sh
@@ -2,17 +2,17 @@
 BASE_PATH=${BASE_PATH:-'/root'}
 
 # Student performance analysis
-# python -m runs.SpeechCommandsV2.PANNs.analysis.std_anal --dataset 'SpeechCommandsV2' \
-#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-#     --output_file 'PAN_SC2-C_L2.csv' --batch_size 32 --corruption_level 'L2' \
-#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
-#     --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
+python -m runs.SpeechCommandsV2.PANNs.analysis.std_anal --dataset 'SpeechCommandsV2' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --output_file 'PAN_SC2-C_L2.csv' --batch_size 32 --corruption_level 'L2' \
+    --orig_wght_pth $BASE_PATH'/result/SpeechCommandsV2/PANNs/train' \
+    --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
 
-# python -m runs.SpeechCommandsV2.PANNs.analysis.std_anal --dataset 'SpeechCommandsV2' \
-#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-#     --output_file 'PAN_SC2-C_L1.csv' --batch_size 32 --corruption_level 'L1' \
-#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
-#     --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
+python -m runs.SpeechCommandsV2.PANNs.analysis.std_anal --dataset 'SpeechCommandsV2' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --output_file 'PAN_SC2-C_L1.csv' --batch_size 32 --corruption_level 'L1' \
+    --orig_wght_pth $BASE_PATH'/result/SpeechCommandsV2/PANNs/train' \
+    --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
 
 # Silhouette analysis
 # python -m runs.SpeechCommandsV2.PANNs.analysis.silhouette_anal --dataset 'SpeechCommandsV2' \
@@ -21,8 +21,8 @@ BASE_PATH=${BASE_PATH:-'/root'}
 #     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
 #     --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
 
-python -m runs.SpeechCommandsV2.PANNs.analysis.silhouette_anal --dataset 'SpeechCommandsV2' \
-    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --output_file 'silhouette_PAN_SC2-C_L1.csv' --batch_size 32 --corruption_level 'L1' \
-    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
-    --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
+# python -m runs.SpeechCommandsV2.PANNs.analysis.silhouette_anal --dataset 'SpeechCommandsV2' \
+#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+#     --output_file 'silhouette_PAN_SC2-C_L1.csv' --batch_size 32 --corruption_level 'L1' \
+#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
+#     --std_adpt_wght_pth './result/SpeechCommandsV2/PANNs/KD'
