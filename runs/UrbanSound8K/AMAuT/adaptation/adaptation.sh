880ee4782cea28aaa4c0fc713cc55d945439caa9
Andy-Shao <hj13035952619@126.com>
2026-06-14 15:01:39 +0000

    optimization

--- runs/UrbanSound8K/AMAuT/adaptation/adaptation.sh
+++ runs/UrbanSound8K/AMAuT/adaptation/adaptation.sh
@@ -2,13 +2,13 @@
 BASE_PATH=${BASE_PATH:-'/root'}
 
 # Teacher Consensus
-python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K' \
-    --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
-    --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
-    --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 2 --max_epoch 30 \
-    --lr '1e-5' --lr_cardinality 60 \
-    --elect_weights '{"WHN":0.9, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --wandb
+# python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
+#     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
+#     --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 2 --max_epoch 30 \
+#     --lr '1e-5' --lr_cardinality 60 \
+#     --elect_weights '{"WHN":0.9, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
+#     --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --wandb
 
 # python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
@@ -19,11 +19,11 @@ python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K'
 #     --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --wandb
 
 # Knowledge Distillation
-# python -m runs.UrbanSound8K.AMAuT.adaptation.kd --dataset 'UrbanSound8K' \
-#     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
-#     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
-#     --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr '1e-4' \
-#     --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 2.92 \
-#     --elect_weights '{"WHN":0.9, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
-#     --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/train' \
-#     --adpt_wght_pth './result/UrbanSound8K/AMAuT/Teach-Cons' --wandb
+python -m runs.UrbanSound8K.AMAuT.adaptation.kd --dataset 'UrbanSound8K' \
+    --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
+    --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
+    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr '1e-4' \
+    --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 2.92 \
+    --elect_weights '{"WHN":0.9, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
+    --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/train' \
+    --adpt_wght_pth './result/UrbanSound8K/AMAuT/Teach-Cons' --wandb
