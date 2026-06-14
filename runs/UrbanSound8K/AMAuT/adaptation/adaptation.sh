8944c41acba8a373140b6e7b72fe6c8090e4e951
Andy-Shao <hj13035952619@126.com>
2026-06-14 13:09:09 +0000

    optimization

--- runs/UrbanSound8K/AMAuT/adaptation/adaptation.sh
+++ runs/UrbanSound8K/AMAuT/adaptation/adaptation.sh
@@ -14,9 +14,9 @@ python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K'
     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
     --batch_size 32 --corruption_level 'L1' --num_of_shft 2 --fail_coll_lim 2 --max_epoch 30 \
-    --lr '1e-4' --lr_cardinality 60 \
-    --elect_weights '{"WHN":1.0, "ENSC":1.0, "PSH":1.0, "TST":1.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --wandb
+    --lr '1e-5' --lr_cardinality 60 \
+    --elect_weights '{"WHN":1.0, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
+    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --wandb
 
 # Knowledge Distillation
 # python -m runs.UrbanSound8K.AMAuT.adaptation.kd --dataset 'UrbanSound8K' \
