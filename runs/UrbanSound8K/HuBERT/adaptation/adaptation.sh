0fed9a5f55e3b4b9a2fd6d581b7950ac8050b7d5
Andy-Shao <hj13035952619@126.com>
2026-06-15 14:18:10 +0000

    optimization

--- runs/UrbanSound8K/HuBERT/adaptation/adaptation.sh
+++ runs/UrbanSound8K/HuBERT/adaptation/adaptation.sh
@@ -13,9 +13,9 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.UrbanSound8K.HuBERT.adaptation.teach_cons --dataset 'UrbanSound8K' \
     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
-    --batch_size 32 --corruption_level 'L1' --num_of_shft 1 --fail_coll_lim 2 --max_epoch 14 --lr '1e-5' \
+    --batch_size 32 --corruption_level 'L1' --num_of_shft 1 --fail_coll_lim 2 --max_epoch 9 --lr '1e-5' \
     --lr_cardinality 80 --lr_threshold 35 \
-    --elect_weights '{"WHN":0.9, "ENSC":1.0, "PSH":1.2, "TST":0.9}' \
+    --elect_weights '{"WHN":0.9, "ENSC":0.9, "PSH":1.2, "TST":1.1}' \
     --lr_momentums '{"WHN":0.9, "ENSC":0.9, "PSH":0.70, "TST":0.9}' \
     --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/TTDA' --wandb
 
