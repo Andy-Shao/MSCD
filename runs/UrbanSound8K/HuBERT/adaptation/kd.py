b4b2f4e3974fac3da2c72986a6740db34884841e
Andy-Shao <hj13035952619@126.com>
2026-06-15 14:49:53 +0000

    optimization

--- runs/UrbanSound8K/HuBERT/adaptation/kd.py
+++ runs/UrbanSound8K/HuBERT/adaptation/kd.py
@@ -228,7 +228,8 @@ if __name__ == '__main__':
         if max_f1 <= f1:
             max_f1 = f1
             store_weight(
-                args=args, hubert=std_hub, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION, root_path=args.output_path
+                args=args, hubert=std_hub, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION, 
+                root_path=args.output_path, metaInfo=CorruptionMeta(type=None, level=args.corruption_level)
             )
         
         if epoch >= args.max_epoch: break
