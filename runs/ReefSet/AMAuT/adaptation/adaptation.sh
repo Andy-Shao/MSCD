66109d2dc2a177b3705f1b0a3cedbfc9d5501b83
Andy-Shao <hj13035952619@126.com>
2026-06-17 14:26:43 +0000

    optimization

--- runs/ReefSet/AMAuT/adaptation/adaptation.sh
+++ runs/ReefSet/AMAuT/adaptation/adaptation.sh
@@ -14,7 +14,7 @@ python -m runs.ReefSet.AMAuT.adaptation.teach_cons --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
     --batch_size 32 --corruption_level 'L1' --max_epoch 10 --lr 1e-4 --num_of_shft 6 --fail_coll_lim 3 \
-    --elect_weights '{"WHN":2.0, "ENQ":1.0, "END1":2.0, "END2":1.0, "ENSC":1.0, "PSH":2.0, "TST":4.0}' \
+    --elect_weights '{"WHN":2.0, "ENQ":1.0, "END1":2.0, "END2":1.0, "ENSC":1.0, "PSH":2.0, "TST":3.0}' \
     --adpt_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/TTDA' --wandb
 
 # Knowledge Distillation
@@ -30,8 +30,8 @@ python -m runs.ReefSet.AMAuT.adaptation.teach_cons --dataset 'ReefSet' \
 # python -m runs.ReefSet.AMAuT.adaptation.kd --dataset 'ReefSet' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
 #     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-#     --batch_size 32 --corruption_level 'L1' --max_epoch 20 --lr 1e-4 --pseudo_threshold 7.69 \
+#     --batch_size 32 --corruption_level 'L1' --max_epoch 20 --lr 1e-4 --pseudo_threshold 7.10 \
 #     --ctr_rt 1.0 --ctr_dist 'sq_l2' --ctr_T 2.0 --lr_momentum 0.75 \
-#     --elect_weights '{"WHN":2.0, "ENQ":1.0, "END1":2.0, "END2":1.0, "ENSC":1.0, "PSH":2.0, "TST":4.0}' \
+#     --elect_weights '{"WHN":2.0, "ENQ":1.0, "END1":2.0, "END2":1.0, "ENSC":1.0, "PSH":2.0, "TST":3.0}' \
 #     --adpt_wght_pth './result/ReefSet/AMAuT/Teach-Cons' \
 #     --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' --wandb
