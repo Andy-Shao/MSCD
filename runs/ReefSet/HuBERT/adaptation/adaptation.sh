b2b4095a3bbb7799b3d0e55bdd8090f388925872
Andy-Shao <hj13035952619@126.com>
2026-06-18 17:11:51 +0000

    optimization

--- runs/ReefSet/HuBERT/adaptation/adaptation.sh
+++ runs/ReefSet/HuBERT/adaptation/adaptation.sh
@@ -2,6 +2,13 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 # Teacher Consensus
+python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 3 --max_epoch 20 \
+    --lr 1e-4 \
+    --elect_weights '{"WHN":1.0, "ENQ":0.9, "END1":1.0, "END2":1.0, "ENSC":0.9, "PSH":1.5, "TST":3.0}' \
+    --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
 # python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
 #     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
@@ -32,11 +39,11 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --orig_wght_pth './result/ReefSet/HuBERT/train' \
 #     --adpt_wght_pth './result/ReefSet/HuBERT/Teach-Cons' --wandb
 
-python -m runs.ReefSet.HuBERT.adaptation.kd --dataset 'ReefSet' \
-    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
-    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L1' --max_epoch 10 --pseudo_threshold 7.15 --ctr_rt 1.0 \
-    --ctr_dist 'sq_l2' --ctr_T 1.0 --lr 1e-5 --model_level 'base' --lr_momentum 0.75 \
-    --elect_weights '{"WHN":0.6, "ENQ":0.9, "END1":1.5, "END2":1.0, "ENSC":1.0, "PSH":3.0, "TST":1.3}' \
-    --orig_wght_pth './result/ReefSet/HuBERT/train' \
-    --adpt_wght_pth './result/ReefSet/HuBERT/Teach-Cons' --wandb
+# python -m runs.ReefSet.HuBERT.adaptation.kd --dataset 'ReefSet' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+#     --batch_size 32 --corruption_level 'L1' --max_epoch 10 --pseudo_threshold 7.15 --ctr_rt 1.0 \
+#     --ctr_dist 'sq_l2' --ctr_T 1.0 --lr 1e-5 --model_level 'base' --lr_momentum 0.75 \
+#     --elect_weights '{"WHN":0.6, "ENQ":0.9, "END1":1.5, "END2":1.0, "ENSC":1.0, "PSH":3.0, "TST":1.3}' \
+#     --orig_wght_pth './result/ReefSet/HuBERT/train' \
+#     --adpt_wght_pth './result/ReefSet/HuBERT/Teach-Cons' --wandb
