9e61c02ffe240f17f8fd1f3d04b832465976b779
Andy-Shao <hj13035952619@126.com>
2026-06-18 15:52:32 +0000

    add analysis

--- runs/ReefSet/AMAuT/analysis/analysis.sh
+++ runs/ReefSet/AMAuT/analysis/analysis.sh
@@ -2,17 +2,17 @@
 BASE_PATH=${BASE_PATH:-'/root'}
 
 # Student Analysis
-# python -m runs.ReefSet.AMAuT.analysis.std_anal --dataset 'ReefSet' \
-#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-#     --batch_size 32 --corruption_level 'L2' --output_file 'AuT_RS-C_L2.csv' \
-#     --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' \
-#     --std_adpt_wght_pth './result/ReefSet/AMAuT/KD'
+python -m runs.ReefSet.AMAuT.analysis.std_anal --dataset 'ReefSet' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --batch_size 32 --corruption_level 'L2' --output_file 'AuT_RS-C_L2.csv' \
+    --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' \
+    --std_adpt_wght_pth './result/ReefSet/AMAuT/KD'
 
-# python -m runs.ReefSet.AMAuT.analysis.std_anal --dataset 'ReefSet' \
-#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-#     --batch_size 32 --corruption_level 'L1' --output_file 'AuT_RS-C_L1.csv' \
-#     --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' \
-#     --std_adpt_wght_pth './result/ReefSet/AMAuT/KD'
+python -m runs.ReefSet.AMAuT.analysis.std_anal --dataset 'ReefSet' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --batch_size 32 --corruption_level 'L1' --output_file 'AuT_RS-C_L1.csv' \
+    --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' \
+    --std_adpt_wght_pth './result/ReefSet/AMAuT/KD'
 
 # Silhouette Analysis
 # python -m runs.ReefSet.AMAuT.analysis.silhouette_anal --dataset 'ReefSet' \
@@ -21,8 +21,8 @@ BASE_PATH=${BASE_PATH:-'/root'}
 #     --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' \
 #     --std_adpt_wght_pth './result/ReefSet/AMAuT/KD'
 
-python -m runs.ReefSet.AMAuT.analysis.silhouette_anal --dataset 'ReefSet' \
-    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L1' --output_file 'silhouette_AuT_RS-C_L1.csv' \
-    --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' \
-    --std_adpt_wght_pth './result/ReefSet/AMAuT/KD'
+# python -m runs.ReefSet.AMAuT.analysis.silhouette_anal --dataset 'ReefSet' \
+#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+#     --batch_size 32 --corruption_level 'L1' --output_file 'silhouette_AuT_RS-C_L1.csv' \
+#     --orig_wght_pth $BASE_PATH'/result/ReefSet/AMAuT/train' \
+#     --std_adpt_wght_pth './result/ReefSet/AMAuT/KD'
