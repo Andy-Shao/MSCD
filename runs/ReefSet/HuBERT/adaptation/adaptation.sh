be48e06248aa2157af1fc9c7b8bb63436f8d225b
Andy-Shao <hj13035952619@126.com>
2026-06-18 20:16:17 +0000

    clean code

--- runs/ReefSet/HuBERT/adaptation/adaptation.sh
+++ runs/ReefSet/HuBERT/adaptation/adaptation.sh
@@ -9,15 +9,6 @@ python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
     --lrs '{"WHN":1e-5, "ENQ":1e-4, "END1":1e-4, "END2":1e-5, "ENSC":1e-4, "PSH":1e-5, "TST":1e-4}' \
     --elect_weights '{"WHN":1.0, "ENQ":0.8, "END1":1.0, "END2":1.0, "ENSC":0.8, "PSH":2.5, "TST":3.0}' \
     --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
-# python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
-#     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
-#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-#     --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 4 --max_epoch 30 \
-#     --lr 1e-5 --lr_momentum 0.75 \
-#     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.2, "END2":1.0, "ENSC":1.0, "PSH":3.0, "TST":1.5}' \
-#     --hub_lr_decaies '{"WHN":0.55, "ENQ":0.55, "END1":0.55, "END2":0.55, "ENSC":0.55, "PSH":0.55, "TST":0.55}' \
-#     --lr_gammas '{"WHN":30, "ENQ":30, "END1":30, "END2":30, "ENSC":30, "PSH":30, "TST":30}' \
-#     --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
 
 # python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
