7e8204f7f824131962f704a58a84ed678eec0a4b
Andy-Shao <hj13035952619@126.com>
2026-06-18 08:49:48 +0000

    optimization

--- runs/ReefSet/AMAuT/adaptation/adaptation.sh
+++ runs/ReefSet/AMAuT/adaptation/adaptation.sh
@@ -5,7 +5,7 @@ BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.ReefSet.AMAuT.adaptation.teach_cons --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 20 \
+    --batch_size 32 --corruption_level 'L2' --max_epoch 15 \
     --forbid_ls 'TST' --lr_gamma 30 --num_of_shft 3 --fail_coll_lim 3 \
     --elect_weights '{"WHN":1.1, "ENQ":1.0, "END1":2.0, "END2":1.1, "ENSC":1.0, "PSH":1.0, "TST":3.0}' \
     --lrs '{"WHN":1e-4, "ENQ":1e-4, "END1":1e-5, "END2":1e-5, "ENSC":1e-4, "PSH":1e-4, "TST":1e-4}' \
