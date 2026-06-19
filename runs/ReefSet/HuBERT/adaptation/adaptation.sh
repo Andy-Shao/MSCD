6be13d7bbe66f0a4b4fc38b00cb82a3dc4014823
Andy-Shao <hj13035952619@126.com>
2026-06-19 15:39:16 +0000

    add HuBERT RS-C L2 teacher consensus

--- runs/ReefSet/HuBERT/adaptation/adaptation.sh
+++ runs/ReefSet/HuBERT/adaptation/adaptation.sh
@@ -2,24 +2,22 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 # Teacher Consensus
-python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
-    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
-    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L2' --num_of_shft 2 --fail_coll_lim 3 --max_epoch 10 \
-    --lrs '{"WHN":1e-5, "ENQ":1e-4, "END1":1e-4, "END2":1e-5, "ENSC":1e-4, "PSH":1e-5, "TST":1e-4}' \
-    --elect_weights '{"WHN":1.0, "ENQ":0.8, "END1":1.0, "END2":1.0, "ENSC":0.8, "PSH":2.5, "TST":3.0}' \
-    --lr_momentums '{"WHN":0.9, "ENQ":0.9, "END1":0.9, "END2":0.9, "ENSC":0.75, "PSH":0.9, "TST":0.9}' \
-    --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
-
 # python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
 #     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-#     --batch_size 32 --corruption_level 'L1' --num_of_shft 3 --fail_coll_lim 3 --max_epoch 20 \
-#     --lr 1e-5 --lr_momentum 0.75 \
-#     --elect_weights '{"WHN":0.6, "ENQ":0.9, "END1":1.5, "END2":1.0, "ENSC":1.0, "PSH":3.0, "TST":1.3}' \
-#     --hub_lr_decaies '{"WHN":0.55, "ENQ":1.0, "END1":0.55, "END2":0.55, "ENSC":1.0, "PSH":1.0, "TST":1.0}' \
-#     --lr_gammas '{"WHN":30, "ENQ":10, "END1":30, "END2":30, "ENSC":10, "PSH":10, "TST":10}' \
+#     --batch_size 32 --corruption_level 'L2' --num_of_shft 2 --fail_coll_lim 3 --max_epoch 10 \
+#     --lrs '{"WHN":1e-5, "ENQ":1e-4, "END1":1e-4, "END2":1e-5, "ENSC":1e-4, "PSH":1e-5, "TST":1e-4}' \
+#     --elect_weights '{"WHN":1.0, "ENQ":0.8, "END1":1.0, "END2":1.0, "ENSC":0.8, "PSH":2.5, "TST":3.0}' \
+#     --lr_momentums '{"WHN":0.9, "ENQ":0.9, "END1":0.9, "END2":0.9, "ENSC":0.75, "PSH":0.9, "TST":0.9}' \
 #     --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
+
+python -m runs.ReefSet.HuBERT.adaptation.teach_cons --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --batch_size 32 --corruption_level 'L1' --num_of_shft 3 --fail_coll_lim 3 --max_epoch 20 \
+    --elect_weights '{"WHN":0.9, "ENQ":1.0, "END1":1.5, "END2":1.0, "ENSC":1.0, "PSH":3.5, "TST":3.0}' \
+    --lrs '{"WHN":1e-4, "ENQ":1e-4, "END1":1e-4, "END2":1e-4, "ENSC":1e-4, "PSH":1e-4, "TST":1e-4}' \
+    --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
 
 # Knowledge Distillation
 # python -m runs.ReefSet.HuBERT.adaptation.kd --dataset 'ReefSet' \
