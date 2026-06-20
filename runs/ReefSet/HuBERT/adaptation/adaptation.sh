a210e8670a5915950f43f3e9cf4f8ed8c2070428
Andy-Shao <hj13035952619@126.com>
2026-06-20 16:14:36 +0000

    optimizaton

--- runs/ReefSet/HuBERT/adaptation/adaptation.sh
+++ runs/ReefSet/HuBERT/adaptation/adaptation.sh
@@ -20,20 +20,20 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --adpt_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/TTDA' --wandb
 
 # Knowledge Distillation
-python -m runs.ReefSet.HuBERT.adaptation.kd --dataset 'ReefSet' \
-    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
-    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 10 --pseudo_threshold 7.77 --ctr_rt 1.0 \
-    --ctr_dist 'sq_l2' --ctr_T 1.0 --lr 1e-4 --model_level 'base' \
-    --elect_weights '{"WHN":1.0, "ENQ":0.8, "END1":1.0, "END2":1.0, "ENSC":0.8, "PSH":2.5, "TST":3.0}' \
-    --orig_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/train' \
-    --adpt_wght_pth './result/ReefSet/HuBERT/Teach-Cons' --wandb
-
 # python -m runs.ReefSet.HuBERT.adaptation.kd --dataset 'ReefSet' \
 #     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
 #     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-#     --batch_size 32 --corruption_level 'L1' --max_epoch 10 --pseudo_threshold 7.15 --ctr_rt 1.0 \
-#     --ctr_dist 'sq_l2' --ctr_T 1.0 --lr 1e-5 --model_level 'base' --lr_momentum 0.75 \
-#     --elect_weights '{"WHN":0.6, "ENQ":0.9, "END1":1.5, "END2":1.0, "ENSC":1.0, "PSH":3.0, "TST":1.3}' \
-#     --orig_wght_pth './result/ReefSet/HuBERT/train' \
+#     --batch_size 32 --corruption_level 'L2' --max_epoch 10 --pseudo_threshold 7.77 --ctr_rt 1.0 \
+#     --ctr_dist 'sq_l2' --ctr_T 1.0 --lr 1e-4 --model_level 'base' \
+#     --elect_weights '{"WHN":1.0, "ENQ":0.8, "END1":1.0, "END2":1.0, "ENSC":0.8, "PSH":2.5, "TST":3.0}' \
+#     --orig_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/train' \
 #     --adpt_wght_pth './result/ReefSet/HuBERT/Teach-Cons' --wandb
+
+python -m runs.ReefSet.HuBERT.adaptation.kd --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --batch_size 32 --corruption_level 'L1' --max_epoch 10 --pseudo_threshold 9.15 --ctr_rt 1.0 \
+    --ctr_dist 'sq_l2' --ctr_T 1.0 --lr 1e-4 --model_level 'base' \
+    --elect_weights '{"WHN":0.9, "ENQ":1.0, "END1":1.5, "END2":1.0, "ENSC":1.0, "PSH":3.5, "TST":3.0}' \
+    --orig_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/train' \
+    --adpt_wght_pth './result/ReefSet/HuBERT/Teach-Cons' --wandb
