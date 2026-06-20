9177fed55537d0e6b0b03cba84578e6262f2dd57
Andy-Shao <hj13035952619@126.com>
2026-06-20 13:24:34 +0000

    optimization

--- runs/ReefSet/HuBERT/adaptation/adaptation.sh
+++ runs/ReefSet/HuBERT/adaptation/adaptation.sh
@@ -23,7 +23,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.ReefSet.HuBERT.adaptation.kd --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --pseudo_threshold 7.77 --ctr_rt 0.0 \
+    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --pseudo_threshold 7.77 --ctr_rt 1.0 \
     --ctr_dist 'sq_l2' --ctr_T 1.0 --lr 1e-4 --model_level 'base' \
     --elect_weights '{"WHN":1.0, "ENQ":0.8, "END1":1.0, "END2":1.0, "ENSC":0.8, "PSH":2.5, "TST":3.0}' \
     --orig_wght_pth $BASE_PATH'/result/ReefSet/HuBERT/train' \
