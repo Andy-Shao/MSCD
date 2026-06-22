d166a2f1068c1f72c33bf02a18e03a7ee622a336
Andy-Shao <hj13035952619@126.com>
2026-06-22 12:05:52 +0000

    optiization

--- runs/ReefSet/PANNs/adaptation/adaptation.sh
+++ runs/ReefSet/PANNs/adaptation/adaptation.sh
@@ -7,7 +7,7 @@ python -m runs.ReefSet.PANNs.adaptation.teach_cons --dataset 'ReefSet' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
     --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 5 --max_epoch 20 \
     --elect_weights '{"WHN":1.0, "ENQ":0.8, "END1":1.0, "END2":0.9, "ENSC":0.8, "PSH":3.0, "TST":1.0}' \
-    --lrs '{"WHN":1e-5, "ENQ":1e-4, "END1":1e-4, "END2":1e-4, "ENSC":5e-5, "PSH":1e-4, "TST":1e-4}' \
+    --lrs '{"WHN":1e-5, "ENQ":1e-4, "END1":1e-4, "END2":1e-5, "ENSC":1e-5, "PSH":1e-4, "TST":1e-4}' \
     --adpt_wght_pth $BASE_PATH'/result/ReefSet/PANNs/TTDA' --freeze_pan --wandb
 
 # Knowledge Distillation
