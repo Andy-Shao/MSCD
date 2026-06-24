ae2011ab24903c67927181722e004a63480de326
Andy-Shao <hj13035952619@126.com>
2026-06-24 09:26:44 +0000

    add max mode

--- runs/VocalSound/AMAuT/adaptation/teach_cons.py
+++ runs/VocalSound/AMAuT/adaptation/teach_cons.py
@@ -20,7 +20,7 @@ from lib.component import Components, AmplitudeToDB, FrequenceTokenTransformer
 from lib.component import MelSpectrogramPadding
 from lib.spSet import VocalSoundC
 from lib.dataset import IdxSet, PseudoLabelSet, Subset
-from lib.adaptation import collect_worst_item, amaut_freeze
+from lib.adaptation import collect_worst_item, amaut_freeze, is_stored
 from ..util import build_model, load_weight, teach_inference, store_weight, partial_freeze
 
 def pseudo_labeling(
@@ -133,6 +133,7 @@ if __name__ == '__main__':
     ap.add_argument('--max_epoch', type=int, default=20)
     ap.add_argument('--forbid_ls', type=str, default="")
     ap.add_argument('--unfrz_pos', type=int, default=-1)
+    ap.add_argument('--max_mode', action='store_true')
 
     ap.add_argument('--lr', type=float, default=1e-2)
     ap.add_argument('--lr_cardinality', type=int, default=40)
@@ -207,7 +208,7 @@ if __name__ == '__main__':
         FrequenceTokenTransformer(),
     ])] * len(corruption_types)
 
-    # max_pseudo_accu = 0.
+    max_pseudo_accu = 0.
     for epoch in range(args.max_epoch+1):
         print(f'Epoch: {epoch+1}/{args.max_epoch} processing...')
         teacher_accu_analyzing(
@@ -218,9 +219,9 @@ if __name__ == '__main__':
             args=args, auts=auts, clsfs=clsfs, corruption_types=corruption_types, 
             data_tfs=data_tfs, step=epoch, logger=wandb_run
         )
-        # if max_pseudo_accu <= pseudo_accu:
-        #     max_pseudo_accu = pseudo_accu
-        for i, corruption_type in enumerate(corruption_types):
+        max_pseudo_accu, store_flag = is_stored(max_val=max_pseudo_accu, curr_val=pseudo_accu, max_mode=args.max_mode)
+        if store_flag:
+            for i, corruption_type in enumerate(corruption_types):
                 store_weight(
                     args=args, aut=auts[i], clsf=clsfs[i], mode='adaptation', 
                     metaInfo=CorruptionMeta(type=corruption_type, level=args.corruption_level),
