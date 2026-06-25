849062aa69ea5c53be8c4b574cc6cd0d08e38234
Andy-Shao <hj13035952619@126.com>
2026-06-25 10:13:46 +0000

    optimization

--- runs/VocalSound/AMAuT/adaptation/adaptation.sh
+++ runs/VocalSound/AMAuT/adaptation/adaptation.sh
@@ -26,4 +26,4 @@ python -m runs.VocalSound.AMAuT.adaptation.kd --dataset 'VocalSound' \
     --pseudo_threshold 6.23 --ctr_dist 'sq_l2' --lr_cardinality 40 \
     --elect_weights '{"WHN":1.3, "ENQ":1.0, "END1":1.3, "END2":1.3, "ENSC":1.0, "PSH":0.9, "TST":1.3}' \
     --orig_wght_pth $BASE_PATH'/result/VocalSound/AMAuT/train' \
-    --adpt_wght_pth './result/VocalSound/AMAuT/Teach-Cons' --wandb
+    --adpt_wght_pth './result/VocalSound/AMAuT/Teach-Cons' --freeze_aut --wandb
