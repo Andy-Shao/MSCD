bf86271c06f288a0cfbedb8189faa3f968abd1e4
Andy-Shao <hj13035952619@126.com>
2026-06-25 09:34:36 +0000

    add KD

--- runs/VocalSound/AMAuT/adaptation/kd.py
+++ runs/VocalSound/AMAuT/adaptation/kd.py
@@ -145,6 +145,7 @@ if __name__ == '__main__':
     ap.add_argument('--lw_def_smth', type=float, default=.2)
     ap.add_argument('--ctr_rt', type=float, default=1.)
     ap.add_argument('--ctr_dist', type=str, default='l2', choices=['cos_sim', 'l2', 'sq_l2'])
+    ap.add_argument('--freeze_aut', action='store_true')
 
     ap.add_argument('--lr', type=float, default=1e-3)
     ap.add_argument('--lr_cardinality', type=int, default=40)
@@ -237,12 +238,13 @@ if __name__ == '__main__':
         if max_accu <= accu:
             max_accu = accu
             store_weight(
-                args=args, aut=std_aut, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION, root_path=args.output_path
+                args=args, aut=std_aut, clsf=std_clsf, mode=constants.STUDENT_ADAPTATION, root_path=args.output_path,
+                metaInfo=CorruptionMeta(type=None, level=args.corruption_level)
             )
         if epoch >= args.max_epoch: break
         print('Adaptating...')
         std_aut.train(); std_clsf.train()
-        amaut_freeze(model=std_aut, drop=False)
+        if args.freeze_aut: amaut_freeze(model=std_aut, drop=False)
         ttl_loss = 0.; ttl_clsf_loss = 0.; ttl_ctr_loss = 0.
         for adpt_data in tqdm(adpt_loader):
             labels = adpt_data[-1].to(args.device)
