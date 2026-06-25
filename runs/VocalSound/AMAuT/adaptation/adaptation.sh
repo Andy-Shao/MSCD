ab493e45ee26c5a8746417dec142953be983a866
Andy-Shao <hj13035952619@126.com>
2026-06-25 11:50:08 +0000

    add L1

--- runs/VocalSound/AMAuT/adaptation/adaptation.sh
+++ runs/VocalSound/AMAuT/adaptation/adaptation.sh
@@ -19,11 +19,20 @@ BASE_PATH=${BASE_PATH:-'/root'}
 #     --adpt_wght_pth $BASE_PATH'/result/VocalSound/AMAuT/TTDA' --freeze_aut --wandb
 
 # Knowledge Distillation
+# python -m runs.VocalSound.AMAuT.adaptation.kd --dataset 'VocalSound' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+#     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+#     --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr '1e-4' --ctr_rt 1.0 \
+#     --pseudo_threshold 6.23 --ctr_dist 'sq_l2' --lr_cardinality 40 \
+#     --elect_weights '{"WHN":1.3, "ENQ":1.0, "END1":1.3, "END2":1.3, "ENSC":1.0, "PSH":0.9, "TST":1.3}' \
+#     --orig_wght_pth $BASE_PATH'/result/VocalSound/AMAuT/train' \
+#     --adpt_wght_pth './result/VocalSound/AMAuT/Teach-Cons' --freeze_aut --wandb
+
 python -m runs.VocalSound.AMAuT.adaptation.kd --dataset 'VocalSound' \
     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-    --batch_size 32 --corruption_level 'L2' --max_epoch 20 --lr '1e-4' --ctr_rt 1.0 \
-    --pseudo_threshold 6.23 --ctr_dist 'sq_l2' --lr_cardinality 40 \
-    --elect_weights '{"WHN":1.3, "ENQ":1.0, "END1":1.3, "END2":1.3, "ENSC":1.0, "PSH":0.9, "TST":1.3}' \
+    --batch_size 32 --corruption_level 'L1' --max_epoch 20 --lr '1e-4' --ctr_rt 0.0 \
+    --pseudo_threshold 6.31 --ctr_dist 'sq_l2' --lr_cardinality 40 \
+    --elect_weights '{"WHN":1.3, "ENQ":1.0, "END1":1.3, "END2":1.0, "ENSC":1.3, "PSH":1.0, "TST":1.3}' \
     --orig_wght_pth $BASE_PATH'/result/VocalSound/AMAuT/train' \
-    --adpt_wght_pth './result/VocalSound/AMAuT/Teach-Cons' --freeze_aut --wandb
+    --adpt_wght_pth './result/VocalSound/AMAuT/Teach-Cons' --wandb
