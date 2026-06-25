8a36245a392b3d0d604d10d4b34ef4d5f4a8f15a
Andy-Shao <hj13035952619@126.com>
2026-06-25 14:00:41 +0000

    optimization

--- runs/VocalSound/AMAuT/adaptation/adaptation.sh
+++ runs/VocalSound/AMAuT/adaptation/adaptation.sh
@@ -31,7 +31,7 @@ BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.VocalSound.AMAuT.adaptation.kd --dataset 'VocalSound' \
     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-    --batch_size 32 --corruption_level 'L1' --max_epoch 20 --lr '1e-4' --ctr_rt 0.0 \
+    --batch_size 32 --corruption_level 'L1' --max_epoch 20 --lr '1e-4' --ctr_rt 1.0 \
     --pseudo_threshold 6.31 --ctr_dist 'sq_l2' --lr_cardinality 40 \
     --elect_weights '{"WHN":1.3, "ENQ":1.0, "END1":1.3, "END2":1.0, "ENSC":1.3, "PSH":1.0, "TST":1.3}' \
     --orig_wght_pth $BASE_PATH'/result/VocalSound/AMAuT/train' \
