c4e47104329be45f39f331bdcb677b94d3b45836
Andy-Shao <hj13035952519@126.com>
2026-06-26 10:37:10 +0000

    optimization

--- runs/VocalSound/HuBERT/adaptation/adaptation.sh
+++ runs/VocalSound/HuBERT/adaptation/adaptation.sh
@@ -8,7 +8,7 @@ python -m runs.VocalSound.HuBERT.adaptation.teach_cons --dataset 'VocalSound' \
     --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 3 --model_level 'base' \
     --max_epoch 20 \
     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.2, "END2":1.2, "ENSC":1.0, "PSH":1.0, "TST":1.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/VocalSound/HuBERT/TTDA'
+    --adpt_wght_pth $BASE_PATH'/result/VocalSound/HuBERT/TTDA' --wandb
 
 # Knowledge Distillation
 # python -m runs.VocalSound.HuBERT.adaptation.kd --dataset 'VocalSound' \
