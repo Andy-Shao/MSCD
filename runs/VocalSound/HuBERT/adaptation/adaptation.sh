8fd49f250b40e0d7d2b8eca9af65f785071b9689
Andy-Shao <hj13035952619@126.com>
2026-06-27 11:49:01 +0000

    optimization

--- runs/VocalSound/HuBERT/adaptation/adaptation.sh
+++ runs/VocalSound/HuBERT/adaptation/adaptation.sh
@@ -15,7 +15,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m runs.VocalSound.HuBERT.adaptation.teach_cons --dataset 'VocalSound' \
     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-    --batch_size 32 --corruption_level 'L1' --num_of_shft 3 --fail_coll_lim 3 --model_level 'base' \
+    --batch_size 32 --corruption_level 'L1' --num_of_shft 2 --fail_coll_lim 3 --model_level 'base' \
     --max_epoch 15 \
     --elect_weights '{"WHN":1.2, "ENQ":1.0, "END1":1.3, "END2":1.3, "ENSC":1.0, "PSH":1.2, "TST":1.0}' \
     --lrs '{"WHN":1e-4, "ENQ":1e-4, "END1":1e-5, "END2":1e-5, "ENSC":1e-4, "PSH":1e-5, "TST":1e-4}' \
