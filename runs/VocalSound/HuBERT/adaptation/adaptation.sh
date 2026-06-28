b3ab2549e213896721127547b58ea5dc134bfc32
Andy-Shao <hj13035952619@126.com>
2026-06-28 13:03:43 +0000

    add L1

--- runs/VocalSound/HuBERT/adaptation/adaptation.sh
+++ runs/VocalSound/HuBERT/adaptation/adaptation.sh
@@ -22,11 +22,20 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --adpt_wght_pth $BASE_PATH'/result/VocalSound/HuBERT/TTDA' --wandb
 
 # Knowledge Distillation
+# python -m runs.VocalSound.HuBERT.adaptation.kd --dataset 'VocalSound' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+#     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+#     --batch_size 32 --corruption_level 'L2' --model_level 'base' --max_epoch 20 --lr 1e-4 \
+#     --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.2, "END2":1.2, "ENSC":1.0, "PSH":1.0, "TST":1.0}' \
+#     --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 5.69 \
+#     --orig_wght_pth $BASE_PATH'/result/VocalSound/HuBERT/train' \
+#     --adpt_wght_pth './result/VocalSound/HuBERT/Teach-Cons' --wandb
+
 python -m runs.VocalSound.HuBERT.adaptation.kd --dataset 'VocalSound' \
     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
-    --batch_size 32 --corruption_level 'L2' --model_level 'base' --max_epoch 20 --lr 1e-4 \
-    --elect_weights '{"WHN":1.0, "ENQ":1.0, "END1":1.2, "END2":1.2, "ENSC":1.0, "PSH":1.0, "TST":1.0}' \
-    --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 5.69 \
+    --batch_size 32 --corruption_level 'L1' --model_level 'base' --max_epoch 20 --lr 1e-4 \
+    --elect_weights '{"WHN":1.2, "ENQ":1.0, "END1":1.3, "END2":1.3, "ENSC":1.0, "PSH":1.2, "TST":1.0}' \
+    --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 6.15 \
     --orig_wght_pth $BASE_PATH'/result/VocalSound/HuBERT/train' \
     --adpt_wght_pth './result/VocalSound/HuBERT/Teach-Cons' --wandb
