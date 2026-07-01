dfe3be88d88e750a9970e47ac9ba3e86d75d26b1
Andy-Shao <hj13035952619@126.com>
2026-07-01 08:43:21 +0000

    fix bugs

--- runs/SpeechCommandsV2/AMAuT/stat_anal/run02.sh
+++ runs/SpeechCommandsV2/AMAuT/stat_anal/run02.sh
@@ -4,7 +4,7 @@ export SEED_VAL=12345
 export LOG_FILE=$BASE_PATH'/MSCD.log'
 export ANAL_FILE_L1='AuT_SC2-C_L2-02.csv'
 export ANAL_FILE_L2='AuT_SC2-C_L1-02.csv'
-export DATASET = 'SC2-C'
+export DATASET='SC2-C'
 
 > $LOG_FILE
 printf 'Processing Log\n' >> $LOG_FILE
