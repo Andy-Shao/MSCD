e6c570fd3379d50508335b13f61e8a7773c138dd
Andy-Shao <hj13035952619@126.com>
2026-07-08 13:48:54 +0000

    fix error

--- runs/UrbanSound8K/AMAuT/stat_anal/run02.sh
+++ runs/UrbanSound8K/AMAuT/stat_anal/run02.sh
@@ -19,7 +19,7 @@ python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K'
     --batch_size 32 --corruption_level 'L2' --num_of_shft 3 --fail_coll_lim 2 --max_epoch 30 \
     --lr '1e-5' --lr_cardinality 60 \
     --elect_weights '{"WHN":0.9, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --wandb
+    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --seed $SEED_VAL
 
 printf 'L1\n' >> $LOG_FILE
 python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K' \
@@ -28,7 +28,7 @@ python -m runs.UrbanSound8K.AMAuT.adaptation.teach_cons --dataset 'UrbanSound8K'
     --batch_size 32 --corruption_level 'L1' --num_of_shft 3 --fail_coll_lim 2 --max_epoch 30 \
     --lr '1e-5' --lr_cardinality 60 \
     --elect_weights '{"WHN":1.0, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
-    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --wandb
+    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/TTDA' --freeze_aut --seed $SEED_VAL
 
 printf 'Knowledge Distillation\n' >> $LOG_FILE
 printf 'L2\n' >> $LOG_FILE
@@ -39,7 +39,7 @@ python -m runs.UrbanSound8K.AMAuT.adaptation.kd --dataset 'UrbanSound8K' \
     --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 2.92 \
     --elect_weights '{"WHN":0.9, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
     --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/train' \
-    --adpt_wght_pth './result/UrbanSound8K/AMAuT/Teach-Cons' --wandb
+    --adpt_wght_pth './result/UrbanSound8K/AMAuT/Teach-Cons' --seed $SEED_VAL
 
 printf 'L1\n' >> $LOG_FILE
 python -m runs.UrbanSound8K.AMAuT.adaptation.kd --dataset 'UrbanSound8K' \
@@ -49,7 +49,7 @@ python -m runs.UrbanSound8K.AMAuT.adaptation.kd --dataset 'UrbanSound8K' \
     --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 3.0 \
     --elect_weights '{"WHN":1.0, "ENSC":1.0, "PSH":0.9, "TST":1.0}' \
     --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/AMAuT/train' \
-    --adpt_wght_pth './result/UrbanSound8K/AMAuT/Teach-Cons' --wandb
+    --adpt_wght_pth './result/UrbanSound8K/AMAuT/Teach-Cons' --seed $SEED_VAL
 
 printf 'Student Analysis\n' >> $LOG_FILE
 python -m runs.UrbanSound8K.AMAuT.analysis.std_anal --dataset 'UrbanSound8K' \
