76477cd23ef8cbf02cd916bca64e6af165633c4a
Andy-Shao <hj13035952619@126.com>
2026-07-08 17:24:36 +0000

    add 02 stat anal config on HuB US8-C

--- runs/UrbanSound8K/HuBERT/stat_anal/run02.sh
+++ runs/UrbanSound8K/HuBERT/stat_anal/run02.sh
@@ -20,7 +20,7 @@ python -m runs.UrbanSound8K.HuBERT.adaptation.teach_cons --dataset 'UrbanSound8K
     --lr_cardinality 80 --lr_threshold 35 \
     --elect_weights '{"WHN":1.0, "ENSC":0.83, "PSH":1.0, "TST":1.0}' \
     --lr_momentums '{"WHN":0.9, "ENSC":0.9, "PSH":0.9, "TST":0.9}' \
-    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/TTDA' --wandb
+    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/TTDA' --seed $SEED_VAL
 
 printf 'L1\n' >> $LOG_FILE
 python -m runs.UrbanSound8K.HuBERT.adaptation.teach_cons --dataset 'UrbanSound8K' \
@@ -30,8 +30,10 @@ python -m runs.UrbanSound8K.HuBERT.adaptation.teach_cons --dataset 'UrbanSound8K
     --lr_cardinality 80 --lr_threshold 35 \
     --elect_weights '{"WHN":0.9, "ENSC":0.9, "PSH":1.2, "TST":1.1}' \
     --lr_momentums '{"WHN":0.9, "ENSC":0.9, "PSH":0.70, "TST":0.9}' \
-    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/TTDA' --wandb
+    --adpt_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/TTDA' --seed $SEED_VAL
 
+printf 'Knowledge Distillation\n' >> $LOG_FILE
+printf 'L1\n' >> $LOG_FILE
 python -m runs.UrbanSound8K.HuBERT.adaptation.kd --dataset 'UrbanSound8K' \
     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
@@ -39,8 +41,9 @@ python -m runs.UrbanSound8K.HuBERT.adaptation.kd --dataset 'UrbanSound8K' \
     --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 2.95 \
     --elect_weights '{"WHN":1.0, "ENSC":0.83, "PSH":1.0, "TST":1.0}' \
     --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/train' \
-    --adpt_wght_pth './result/UrbanSound8K/HuBERT/Teach-Cons' --wandb
+    --adpt_wght_pth './result/UrbanSound8K/HuBERT/Teach-Cons' --seed $SEED_VAL
 
+printf 'L2\n' >> $LOG_FILE
 python -m runs.UrbanSound8K.HuBERT.adaptation.kd --dataset 'UrbanSound8K' \
     --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
@@ -48,4 +51,21 @@ python -m runs.UrbanSound8K.HuBERT.adaptation.kd --dataset 'UrbanSound8K' \
     --ctr_rt 1.0 --ctr_dist 'sq_l2' --pseudo_threshold 3.15 \
     --elect_weights '{"WHN":0.9, "ENSC":0.9, "PSH":1.2, "TST":1.1}' \
     --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/train' \
-    --adpt_wght_pth './result/UrbanSound8K/HuBERT/Teach-Cons' --wandb
+    --adpt_wght_pth './result/UrbanSound8K/HuBERT/Teach-Cons' --seed $SEED_VAL
+
+printf 'Student Analysis\n' >> $LOG_FILE
+printf 'L2\n' >> $LOG_FILE
+python -m runs.UrbanSound8K.HuBERT.analysis.std_anal --dataset 'UrbanSound8K' \
+    --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
+    --output_file $ANAL_FILE_L2 --batch_size 32 --corruption_level 'L2' --model_level 'base' \
+    --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/train' \
+    --std_adpt_wght_pth './result/UrbanSound8K/HuBERT/KD'
+
+printf 'L1\n' >> $LOG_FILE
+python -m runs.UrbanSound8K.HuBERT.analysis.std_anal --dataset 'UrbanSound8K' \
+    --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
+    --output_file $ANAL_FILE_L1 --batch_size 32 --corruption_level 'L1' --model_level 'base' \
+    --orig_wght_pth $BASE_PATH'/result/UrbanSound8K/HuBERT/train' \
+    --std_adpt_wght_pth './result/UrbanSound8K/HuBERT/KD'
+
+printf 'ALL processing is finished\n' >> $LOG_FILE
