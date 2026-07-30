092d3678abb5336c34bf8a74b15038765c9a256d
Andy-Shao <hj13035952619@126.com>
2026-07-30 13:18:04 +0000

    add No TC analysis on HuB RS-C

---
+++ ablation_study/No_TC/ReefSet/HuBERT/stat_anal.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L1.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L1-02.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L1-03.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L1-04.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L1-05.csv"
+
+python -m runs.stat_anal --output_file_name 'HuB_RS-C_L1_stat_anal.csv' \
+    --output_path './result/ReefSet/HuBERT/ablation_study/No_TC' \
+    --report_list "${report_list}" --dataset 'RS-C' --arch 'HuBERT'
+
+report_list="\
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L2.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L2-02.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L2-03.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L2-04.csv, \
+./result/ReefSet/HuBERT/ablation_study/No_TC/HuB_RS-C_L2-05.csv"
+
+python -m runs.stat_anal --output_file_name 'HuB_RS-C_L2_stat_anal.csv' \
+    --output_path './result/ReefSet/HuBERT/ablation_study/No_TC' \
+    --report_list "${report_list}" --dataset 'RS-C' --arch 'HuBERT'
