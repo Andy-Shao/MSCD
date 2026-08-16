821739b306324db18ee5b781642d1cabf035db76
Andy-Shao <hj13035952619@126.com>
2026-08-16 14:38:00 +0000

    add No BN analysis on AuT US8-C

---
+++ ablation_study/No_BN/UrbanSound8K/AMAuT/stat_anal.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L1.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L1-02.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L1-03.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L1-04.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L1-05.csv"
+
+python -m runs.stat_anal --output_file_name 'AuT_US8-C_L1_stat_anal.csv' \
+    --output_path './result/UrbanSound8K/AMAuT/ablation_study/No_BN' \
+    --report_list "${report_list}" --dataset 'US8-C' --arch 'AMAuT'
+
+report_list="\
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L2.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L2-02.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L2-03.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L2-04.csv, \
+./result/UrbanSound8K/AMAuT/ablation_study/No_BN/AuT_US8-C_L2-05.csv"
+
+python -m runs.stat_anal --output_file_name 'AuT_US8-C_L2_stat_anal.csv' \
+    --output_path './result/UrbanSound8K/AMAuT/ablation_study/No_BN' \
+    --report_list "${report_list}" --dataset 'US8-C' --arch 'AMAuT'
