2d3bd245839c7256b3517d0e9e5fdf5eb0a10dd6
Andy-Shao <hj13035952619@126.com>
2026-08-18 14:17:08 +0000

    add No BN analysis on PAN US8-C

---
+++ ablation_study/No_BN/UrbanSound8K/PANNs/stat_anal.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L1.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L1-02.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L1-03.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L1-04.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L1-05.csv"
+
+python -m runs.stat_anal --output_file_name 'PAN_US8-C_L1_stat_anal.csv' \
+    --output_path './result/UrbanSound8K/PANNs/ablation_study/No_BN' \
+    --report_list "${report_list}" --dataset 'US8-C' --arch 'PANNs'
+
+report_list="\
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L2.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L2-02.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L2-03.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L2-04.csv, \
+./result/UrbanSound8K/PANNs/ablation_study/No_BN/PAN_US8-C_L2-05.csv"
+
+python -m runs.stat_anal --output_file_name 'PAN_US8-C_L2_stat_anal.csv' \
+    --output_path './result/UrbanSound8K/PANNs/ablation_study/No_BN' \
+    --report_list "${report_list}" --dataset 'US8-C' --arch 'PANNs'
