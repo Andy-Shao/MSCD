b489839aace8e1cbe03122c788706b0bc48fd82c
Andy-Shao <hj13035952619@126.com>
2026-08-24 15:27:32 +0000

    add No BN analysis on PAN VS-C

---
+++ ablation_study/No_BN/VocalSound/PANNs/stat_anal.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L1.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L1-02.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L1-03.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L1-04.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L1-05.csv"
+
+python -m runs.stat_anal --output_file_name 'PAN_VS-C_L1_stat_anal.csv' \
+    --output_path './result/VocalSound/PANNs/ablation_study/No_BN' \
+    --report_list "${report_list}" --dataset 'VS-C' --arch 'PANNs'
+
+report_list="\
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L2.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L2-02.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L2-03.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L2-04.csv, \
+./result/VocalSound/PANNs/ablation_study/No_BN/PAN_VS-C_L2-05.csv"
+
+python -m runs.stat_anal --output_file_name 'PAN_VS-C_L2_stat_anal.csv' \
+    --output_path './result/VocalSound/PANNs/ablation_study/No_BN' \
+    --report_list "${report_list}" --dataset 'VS-C' --arch 'PANNs'
