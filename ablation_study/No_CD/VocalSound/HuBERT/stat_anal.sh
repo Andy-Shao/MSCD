fa76a5e0d3ea0032976043123ff7c05f8f86a7df
Andy-Shao <hj13035952619@126.com>
2026-09-05 21:26:04 +0000

    add No CD analysis on HuB VS-C

---
+++ ablation_study/No_CD/VocalSound/HuBERT/stat_anal.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L1.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L1-02.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L1-03.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L1-04.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L1-05.csv"
+
+python -m runs.stat_anal --output_file_name 'HuB_VS-C_L1_stat_anal.csv' \
+    --output_path './result/VocalSound/HuBERT/ablation_study/No_CD' \
+    --report_list "${report_list}" --dataset 'VS-C' --arch 'HuBERT'
+
+report_list="\
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L2.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L2-02.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L2-03.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L2-04.csv, \
+./result/VocalSound/HuBERT/ablation_study/No_CD/HuB_VS-C_L2-05.csv"
+
+python -m runs.stat_anal --output_file_name 'HuB_VS-C_L2s_stat_anal.csv' \
+    --output_path './result/VocalSound/HuBERT/ablation_study/No_CD' \
+    --report_list "${report_list}" --dataset 'VS-C' --arch 'HuBERT'
