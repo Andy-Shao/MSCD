054fc6792a61f28f382465af7f1b88f0f926f615
Andy-Shao <hj13035952619@126.com>
2026-09-06 11:42:35 +0000

    add 04 No CE config on AuT US8-C

--- ablation_study/No_CE/UrbanSound8K/AMAuT/run04.sh
+++ ablation_study/No_CE/UrbanSound8K/AMAuT/run04.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
-export SEED_VAL=12345
+export SEED_VAL=56789
 export LOG_FILE=$BASE_PATH'/MSCD.log'
-export ANAL_FILE_L1='AuT_US8-C_L1.csv'
-export ANAL_FILE_L2='AuT_US8-C_L2.csv'
+export ANAL_FILE_L1='AuT_US8-C_L1-04.csv'
+export ANAL_FILE_L2='AuT_US8-C_L2-04.csv'
 export DATASET='US8-C'
 
 > $LOG_FILE
