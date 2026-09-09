2bea1009506ea597c762d6b516198e67a3f702fa
Andy-Shao <hj13035952619@126.com>
2026-09-09 13:32:45 +0000

    add No CE analysis on PAN RS-C

---
+++ ablation_study/No_CE/ReefSet/PANNs/stat_anal.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L1.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L1-02.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L1-03.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L1-04.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L1-05.csv"
+
+python -m runs.stat_anal --output_file_name 'PAN_RS-C_L1_stat_anal.csv' \
+    --output_path './result/ReefSet/PANNs/ablation_study/No_CE' \
+    --report_list "${report_list}" --dataset 'RS-C' --arch 'PANNs'
+
+report_list="\
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L2.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L2-02.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L2-03.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L2-04.csv, \
+./result/ReefSet/PANNs/ablation_study/No_CE/PAN_RS-C_L2-05.csv"
+
+python -m runs.stat_anal --output_file_name 'PAN_RS-C_L2_stat_anal.csv' \
+    --output_path './result/ReefSet/PANNs/ablation_study/No_CE' \
+    --report_list "${report_list}" --dataset 'RS-C' --arch 'PANNs'
